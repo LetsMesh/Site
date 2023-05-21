acfa5c6d3743e6b2a7d0a017ec470d09e16d8239
bluesimp1102 <jackismyname7@gmail.com>
2023-05-21 13:32:41 -0700

    handle exceptions

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import Accounts
 from django.core import serializers
 import json
+from ..views import validate_data
 
 def get_all_accounts(request):
     if request.method == 'GET':
@@ -13,7 +14,8 @@ def get_all_accounts(request):
 @csrf_exempt
 def create_account(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
+        REQUIRED_FIELDS = ['email', 'encryptedPass', 'phoneNum', 'displayTheme', 'enabled2Factor']
+        data = validate_data(request.body, REQUIRED_FIELDS)
         account = Accounts.objects.create(
             email = data['email'], 
             encryptedPass = data['encryptedPass'], 
@@ -25,9 +27,13 @@ def create_account(request):
 
 @csrf_exempt
 def update_account(request, account_id):
-    if request.method == 'PUT':
-        account = Accounts.objects.get(id=account_id)
-        data = json.loads(request.body)
+    if request.method == 'PATCH':
+        try:
+            account = Accounts.objects.get(id = account_id)
+        except Accounts.DoesNotExist:
+            return HttpResponse(status = 404) # return error if the account_id is not valid
+        
+        data = validate_data(request.body)
         account.email = data.get('email', account.email)
         account.encryptedPass = data.get('encryptedPass', account.encryptedPass)
         account.phoneNum = data.get('phoneNum', account.phoneNum)
