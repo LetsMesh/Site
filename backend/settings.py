782c6cd30693a8ce7ea3cfc9f7ec55c00c402f84
bluesimp1102 <jackismyname7@gmail.com>
2023-05-20 23:46:06 -0700

    updated DATABASES
    I was not sure that what kind of database we were going to used exactly so I went with my local mysql database as a temp. solution.

--- backend/settings.py
+++ backend/settings.py
@@ -85,9 +85,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),  # Or the appropriate hostname if not localhost
+        'PORT': os.environ.get('DB_PORT'),       # Default port for MySQL
     }
 }
 
