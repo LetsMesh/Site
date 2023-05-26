018f6714cd2d153f95ff2104139f50fad140de71
peppacaiou <109382836+peppacaiou@users.noreply.github.com>
2023-05-25 21:02:28 -0700

    changed the databse in settings.py to be mysql instead oif sqlite3

--- backend/settings.py
+++ backend/settings.py
@@ -85,8 +85,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'user_data',
+        'ENGINE': 'mysql.connector.django',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'mysql_user',
+        'PASSWORD': 'password',
+        'OPTIONS': {
+          'autocommit': True,
+          'use_oure': True,
+          'init_command': "SET foo='bar';"
+        },
     }
 }
 
