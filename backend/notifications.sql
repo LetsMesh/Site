745494701b78ab87b53bac0b7548d4701e2eda8d
William Sosropartono <wcsosropartono@gmail.com>
2023-05-29 12:05:36 -0700

    Changed accountID to not auto increment

--- backend/notifications.sql
+++ backend/notifications.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Notifications(
     notificationID INT NOT NULL AUTO_INCREMENT,
-    accountID INT NOT NULL AUTO_INCREMENT,
+    accountID INT NOT NULL,
     timestamp TIMESTAMP NOT NULL,
     notificationContextJson VARCHAR(255) NOT NULL,
     PRIMARY KEY (notificationID),
