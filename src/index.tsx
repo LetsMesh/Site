8493e662eec9eccebee23f4fcb7c4c6350f6ae84
owwix <53359125+owwix@users.noreply.github.com>
2023-05-29 16:23:01 -0700

    basic password reset form implemented

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Login from './pages/Login';
+import  Login from './pages/Login';
+import { PasswordReset } from './pages/PasswordReset'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
