946cda7eefb41c9b9adf420abf794ab8f9df321c
William Sosropartono <wcsosropartono@gmail.com>
2023-05-29 12:29:13 -0700

    Changed django ver, removed auto increment

--- backend/tags-bridge.sql
+++ backend/tags-bridge.sql
@@ -1,6 +1,6 @@
 CREATE TABLE TagsBridge(
-    accountID INT NOT NULL AUTO_INCREMENT,
     tagID INT NOT NULL AUTO_INCREMENT,
+    accountID INT NOT NULL,
     PRIMARY KEY (accountID, tagID),
     FOREIGN KEY (accountID) REFERENCES Profiles (AccountID),
     FOREIGN KEY (tagID) REFERENCES Tags (tagID)
