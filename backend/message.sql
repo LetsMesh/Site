27b2b7c57c8a6416f7be8a189341b3c3f18c35da
Koki <kyamaguchi@cpp.edu>
2023-05-29 15:59:42 -1000

    added message table and SQL query

---
+++ backend/message.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Messages (
+    messageID INT PRIMARY KEY,
+    fromAccountID INT,
+    toAccountID INT,
+    message TEXT,
+    timestamp TIMESTAMP,
+    FOREIGN KEY (fromAccountID) REFERENCES Accounts(accountID),
+    FOREIGN KEY (toAccountID) REFERENCES Accounts(accountID)
+);
