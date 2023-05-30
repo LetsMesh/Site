a1d125a7d3cf64a72a843bcfa764f0b50a0d4fb4
MoneyEt <ethanngo3539@gmail.com>
2023-05-30 14:12:02 -0700

    Email verification page

--- meshapp/src/components/SignUp/StepTwo.tsx
+++ meshapp/src/components/SignUp/StepTwo.tsx
@@ -1,5 +1,38 @@
+import { Avatar, Button, Grid, Typography } from "@mui/material";
+
+const signUpStyle = {
+    paddingTop: 15,
+    paddingBottom: 15
+}
+
 export default function StepTwo(){
     return(
-        <></>
+        <>
+                <Grid container style={signUpStyle}>
+                    <Grid item xs = {6}>
+                        <Grid container direction="column" alignItems={'center'}>
+                            <Grid item>
+                                <Typography variant="h3">Verify Your Email</Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="body1">Why do we verify?To ensure that the email
+                                you provided can be accessed by you so we can send you more memes</Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="h4">email@address.com</Typography>
+                                <Button variant="contained" color='success'>Send Verify Link</Button>
+                            </Grid>
+                        </Grid>
+                    </Grid> 
+                    <Grid item xs = {6}>
+                        <Grid item>
+                            <Avatar alt="Email icon"
+                            src="email.png"
+                            sx={{width: 285, height: 281}}
+                            />
+                        </Grid>
+                    </Grid> 
+                </Grid>
+        </>
     )
 }
