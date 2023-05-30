c4daacc50982f83b6998b650eadaa7fb81ff6283
calhuy <calvinhooyen@gmail.com>
2023-05-29 19:25:51 -0700

    Added content into banner, edited banner.css for positioning elements

---
+++ src/2FA/2FA-Banner.tsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+import { Modal, Button, Typography, Fade, Backdrop } from "@mui/material";
+
+import CloseIcon from "@mui/icons-material/Close";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { ReactComponent as Logo } from "./2FA-icon.svg";
+
+
+import "./Banner.css";
+
+
+
+
+function TwoFactorBanner(props: any) {
+
+    return(
+        <div 
+            className="banner"
+            style={{ width: props.width, height: 50}}
+        >
+
+            <div className="flexbox-container">
+
+                <div className="bannerLogo">
+                <Logo
+                    style={{
+                    width: "50px",
+                    height: "50px"
+                    }}
+                />
+                </div>
+
+                <div className="descText">
+                <Typography sx={{ fontSize: 16, fontFamily: "cocogoose" }}>
+                    Secure your account with Two-Factor Authentication.
+                </Typography>
+                </div>
+
+                <div className="bannerBtn">
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        sx={{
+                            backgroundColor: "#0B7D66",
+                            fontFamily: "cocogoose",
+                            "&:hover": {
+                            backgroundColor: "#0A6B57",
+                            },
+                            borderRadius: 3,
+                        }}
+                        endIcon={<ChevronRightIcon />}
+                        onClick={props.handleOptIn}
+                        >
+                        GO TO SETTINGS
+                    </Button>
+                <div/>
+
+                <div className="bannerCloseBtn">
+                    <CloseIcon
+                    sx={{
+                        color: "#848484",
+                        "&:hover": {
+                        color: "black",
+                        },
+                        cursor: "pointer",
+                        transition: "color 0.15s ease-in-out",
+                    }}
+                    fontSize="small"
+                    onClick={props.onClose}
+                    />
+                </div>
+
+
+
+            </div>
+
+            </div>
+
+        </div>
+    );
+
+}
+
+export default TwoFactorBanner
--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import "./App.css";
 import React from "react";
 
 import TwoFactorAuthModal from "./2FA/2FA-Popup";
+import TwoFactorBanner from "./2FA/2FA-Banner";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <TwoFactorBanner />
         <TwoFactorAuthModal />
       </header>
     </div>
