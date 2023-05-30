dcedfd1c2a0fa1aa018cd82d10943fb492f191ef
peppacaiou <109382836+peppacaiou@users.noreply.github.com>
2023-05-29 22:44:50 -0700

    fixed mysql errors

--- backend/accounts.sql
+++ backend/accounts.sql
@@ -1,13 +1,12 @@
 -- @block 
-CREATE TABLE IF NOT EXISTS account (
-    accountID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS Accounts (
+    accountID INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(254) NOT NULL,
     encryptedPass VARCHAR(255) NOT NULL,
-    displayTheme int NOT NULl,
-    phoneNum VARCHAR(20),
-    enable2Factor BOOLEAN,
+    displayTheme ENUM("light","dark") NOT NULL,
+    phoneNum VARCHAR(15),
+    is2FAEnabled BOOLEAN DEFAULT 0,
     isMentee BOOLEAN NOT NULL,
     isMentor BOOLEAN NOT NULL
 
 );
-
--- backend/profileTable.sql
+++ backend/profileTable.sql
@@ -1,11 +1,11 @@
 -- @block
-CREATE TABLE IF NOT EXISTS profileTable(
-    accountID INT NOT NULL,
+CREATE TABLE IF NOT EXISTS Profiles(
+    accountID INT ,
     userName VARCHAR(255) NOT NULL,
     preferredName VARCHAR(255),
-    preferredPronun VARCHAR(255),
+    preferredPronuns VARCHAR(40),
     biography TEXT(500),
-    profilePicture VARCHAR(255) NOT NULL,
+    linkToprofilePicture VARCHAR(255) NOT NULL,
 
     FOREIGN KEY (accountID) references account(accountID),
     PRIMARY KEY(accountID)
