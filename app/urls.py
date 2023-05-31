3b0847c696dbfbb8bdd62af40b773466716b9668
bluesimp1102 <jackismyname7@gmail.com>
2023-05-30 22:51:19 -0700

    fix the path and module

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    # path('accounts/', include('accounts.urls')),
+    path('accounts/', include('app.accounts.urls')),
 ]
