4eecc01a2852d94b6a421aacc3f9f9316e928098
PenguGH <carterphung13@gmail.com>
2023-06-01 14:07:25 -0700

    "added FooterStyles.js"

---
+++ src/FooterStyles.js
@@ -0,0 +1,63 @@
+// need to convert to typescript
+
+import styled from 'styled-components';
+
+export const Box = styled.div`
+  padding: 50px 50px;
+  background: #f2e8de;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  
+   
+  @media (max-width: 1000px) {
+    padding: 70px 30px;
+  }
+`;
+   
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    max-width: 1000px;
+    margin: 0 auto;
+    /* background: red; */
+`
+   
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-left: 60px;
+`;
+   
+export const Row = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 
+                         minmax(185px, 1fr));
+  grid-gap: 20px;
+   
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(auto-fill, 
+                           minmax(200px, 1fr));
+  }
+`;
+   
+export const FooterLink = styled.a`
+  color: black;
+  margin-bottom: 20px;
+  font-size: 19px;
+  text-decoration: none;
+   
+  &:hover {
+      color: #1db272;
+      transition: 200ms ease-in;
+  }
+`;
+   
+export const Heading = styled.p`
+  font-size: 20px;
+  color: black;
+  margin-bottom: 30px;
+  font-weight: bold;
+`;
