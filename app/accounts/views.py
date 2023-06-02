1984c3f8d40b9684d07686cb36ea1c0a5aa59044
bluesimp1102 <jackismyname7@gmail.com>
2023-06-02 00:45:20 -0700

    add comments and documentation

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -6,18 +6,31 @@ from django.views import View
 
 from .models import Accounts
 from ..utils.validate_data import validate_data
-from ..middlewares.exceptions import CustomException
 import json
+
 
 @method_decorator(csrf_exempt, name='dispatch')
 class AccountView(View):
+    """
+    View for listing all Accounts or creating a new Account.
+    """
     def get(self, request, *args, **kwargs):
-        print('request get all accounts')
+        """
+        Handle GET requests.
+
+        Returns a JSON response containing all Accounts.
+        """
         accounts = Accounts.objects.all()
         accounts_list = serializers.serialize('json', accounts)
         return JsonResponse(accounts_list, safe=False)
 
     def post(self, request, *args, **kwargs):
+        """
+        Handle POST requests.
+
+        Validates request data against REQUIRED_FIELDS and creates a new Account. 
+        Returns a JSON response with the newly created account's ID and a 201 status code.
+        """
         REQUIRED_FIELDS = ['email', 'encryptedPass', 'phoneNum', 'displayTheme', 'enabled2Factor', 'isMentor', 'isMentee']
         data = validate_data(request.body, REQUIRED_FIELDS)
         account = Accounts.objects.create(
@@ -29,12 +42,21 @@ class AccountView(View):
             isMentor = data['isMentor'],
             isMentee = data['isMentee']
         )
-        return JsonResponse({'account_id': account.id}, status=201)  # Return a 201 status code
+        return JsonResponse({'account_id': account.id}, status=201)
+
 
 @method_decorator(csrf_exempt, name='dispatch')
 class AccountDetailView(View):
-
+    """
+    View for getting an Account by ID or updating an Account.
+    """
     def get(self, request, account_id, *args, **kwargs):
+        """
+        Handle GET requests.
+
+        Returns a JSON response containing the Account with the given ID.
+        If the Account does not exist, it returns a 404 status code and an error message.
+        """
         try:
             account = Accounts.objects.get(id=account_id)
             account_detail = serializers.serialize('json', [account])
@@ -43,6 +65,22 @@ class AccountDetailView(View):
             return JsonResponse({'error': 'Account does not exist'}, status=404)
 
     def patch(self, request, account_id, *args, **kwargs):
+        """
+        Handle PATCH requests.
+
+        Updates the specified fields of the Account with the given ID.
+        If the Account does not exist, it returns a 404 status code and an error message.
+
+        Updates the account information with data from the request body. 
+
+        If a field is not provided in the request body, the current value for 
+        that field will remain unchanged. 
+
+        After updating the account information, it saves the changes to the database.
+
+        Returns a 204 HTTP status to indicate that the request has succeeded 
+        but does not include an entity-body in the response.
+        """
         try:
             account = Accounts.objects.get(id=account_id)
         except Accounts.DoesNotExist:
