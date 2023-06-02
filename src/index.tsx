2010dd87e178a23de2173aed09b1f3a86eda719d
Darren <98421648+Darren52BT@users.noreply.github.com>
2023-06-02 11:31:27 -0700

    close #135 : F3.2 Implement UI/UX for Logged Out Home Screen.
    * basic outline of logged out page
    
    * added nav bar, reorganized components into diff files
    
    * added font size/flex box alignments for certain screen widths
    
    * formatted code a little
    
    * removed the login form I grabbed from leaversas branch, will import and put it back when his branch gets merged
    
    * set the page to render automatically
    
    * formatted with prettier, added hover color to buttons
    
    * made hover button colors lighter
    
    * moved images to images folder within src folder
    
    * Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import TwoFactorAuthReminders from "./2FA/2FA-Remind";
+import LoggedOutPage from "./pages/loggedOutPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
