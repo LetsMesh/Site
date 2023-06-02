6f26dc00825c11f16e7be5736a87990e641e891e
Darren52BT <darrenbt52@gmail.com>
2023-06-01 21:40:42 -0700

    set the page to render automatically

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import LoggedOutPage from './pages/loggedOutPage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
-    <App />
+    <LoggedOutPage />
   </React.StrictMode>
 );
 
