15a3f3a0c20ae7d965f117c4149f96e2e7874450
PattyCakesxD <nguyenpatty42@gmail.com>
2023-06-01 20:00:22 -0700

    adds a slide effect on closing banner

--- src/2FA/2FA-Banner.tsx
+++ src/2FA/2FA-Banner.tsx
@@ -5,71 +5,67 @@ import CloseIcon from "@mui/icons-material/Close";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { ReactComponent as Logo } from "./2FA-icon.svg";
 
-
 import "./Banner.css";
-
-
 
 function TwoFactorBanner(props: any) {
   const [visible, setVisible] = useState(true);
 
   const removeElement = () => {
-    setVisible((prev) => !prev);
+    setVisible(!visible);
   };
 
   return (
-    <div className="banner"
-    style={{visibility: visible ? 'visible' : 'hidden' }}>
-      <div className="bannerLogo">
-        <Logo
-          style={{
-            width: "45px",
-            height: "45px",
-          }}
-        />
-      </div>
+    <Slide direction="down" in={visible} mountOnEnter unmountOnExit>
+      <div className="banner">
+        <div className="bannerLogo">
+          <Logo
+            style={{
+              width: "45px",
+              height: "45px",
+            }}
+          />
+        </div>
 
-      <div className="descText">
-        <Typography sx={{ fontSize: 17, fontFamily: "cocogoose" }}>
-          Secure your account with Two-Factor Authentication
-        </Typography>
-      </div>
+        <div className="descText">
+          <Typography sx={{ fontSize: 17, fontFamily: "cocogoose" }}>
+            Secure your account with Two-Factor Authentication
+          </Typography>
+        </div>
 
-      <div className="bannerBtn">
-        <Button
-          fullWidth
-          variant="contained"
-          sx={{
-            fontSize: 16,
-            backgroundColor: "#0B7D66",
-            fontFamily: "cocogoose",
-            "&:hover": {
-              backgroundColor: "#0A6B57",
-            },
-            borderRadius: 3,
-          }}
-          endIcon={<ChevronRightIcon />}
-          onClick={props.handleOptIn}
-        >
-          GO TO SETTINGS
-        </Button>
-        <div />
-      </div>
+        <div className="bannerBtn">
+          <Button
+            fullWidth
+            variant="contained"
+            sx={{
+              fontSize: 16,
+              backgroundColor: "#0B7D66",
+              fontFamily: "cocogoose",
+              "&:hover": {
+                backgroundColor: "#0A6B57",
+              },
+              borderRadius: 3,
+            }}
+            endIcon={<ChevronRightIcon />}
+            onClick={props.handleOptIn}
+          >
+            GO TO SETTINGS
+          </Button>
+          <div />
+        </div>
 
-      <div className="bannerCloseBtn">
-        <CloseIcon
-          sx={{
-            color: "black",
-            cursor: "pointer",
-            transition: "color 0.15s ease-in-out",
-          }}
-          fontSize="small"
-          
-          onClick={removeElement}
-
-        />
+        <div className="bannerCloseBtn">
+          <CloseIcon
+            sx={{
+              color: "black",
+              cursor: "pointer",
+              transition: "color 0.15s ease-in-out",
+            }}
+            fontSize="small"
+            onClick={removeElement}
+          />
+        </div>
       </div>
-    </div>
+    </Slide>
   );
 }
 
