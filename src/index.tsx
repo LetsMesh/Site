5d2f3420969d7d9cb369b30da4717ce1a5e6ba79
Darren <98421648+Darren52BT@users.noreply.github.com>
2023-06-02 11:28:50 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <TwoFactorAuthReminders />
-    <LoggedOutPage />
   </React.StrictMode>
 );
 
