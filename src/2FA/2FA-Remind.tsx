20eff70d910073f5760cb71a98f666d026233a46
Patrick Hoang <nguyenpatty42@gmail.com>
2023-06-02 11:11:11 -0700

    #76: B3.2 Implements 2FA opt-in UI prompts on login
    * setup for components
    
    * adds modal button for testing
    
    * adds a close button
    
    * adds UI onto modal
    
    * adds fade effect on open/close
    
    * change hover effect of x button
    
    * remove redundant code
    
    * implements first-time popup effect
    
    * some UI fixes
    
    * clarifying comment
    
    * refactor UI of 2FAModal for scalability
    
    * gives opt-in button more rounded corners
    
    * removes closing modal by clicking backdrop
    
    * Added content into banner, edited banner.css for positioning elements
    
    * Fixed spacing of banner elements
    
    * refactor code to prettier format
    
    * remove unused imports
    
    * refactor banner styling
    
    * fix center styling of some elements
    
    * Added banner shadow to css, and added banner close button functionality
    
    * adds a slide effect on closing banner
    
    * comments out open modal button
    
    * makes banner shadow more apparent
    
    * wrap both components into one file and finish localstorage logic
    
    * remove unused imports
    
    * shrinks banner logo a bit and move text 2px down
    
    * adds javadocs for all relevant components
    
    ---------
    
    Co-authored-by: calhuy <calvinhooyen@gmail.com>

---
+++ src/2FA/2FA-Remind.tsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+
+import TwoFactorAuthModal from "./2FA-Popup";
+import TwoFactorBanner from "./2FA-Banner";
+
+import "./TempBody.css";
+
+/**
+ * A React component used to remind the user about enabling Two-Factor Authentication (2FA) in their settings.
+ * Local storage is used to determine whether to show the reminding modal or banner.
+ *
+ * If a user hasn't been prompted to enable 2FA and it's not already enabled, a modal will be shown on page load.
+ * If the user has been prompted but hasn't enabled 2FA yet, a banner will be shown on page load.
+ *
+ * It manages two important pieces of UI:
+ * 1. A modal (TwoFactorAuthModal) that prompts users to enable 2FA.
+ * 2. A banner (TwoFactorBanner) that reminds users about enabling 2FA.
+ */
+const TwoFactorAuthReminders = () => {
+  // Used to track popup's visibility
+  const [modalVisible, setModalVisible] = useState(false);
+  const handleModalOpen = () => setModalVisible(true);
+  const handleModalClose = () => {
+    setModalVisible(false);
+    localStorage.setItem("promptedFor2FA", "true");
+
+    // Upon closing the modal, show the banner
+    handleBannerOpen();
+  };
+
+  // Used to track banner's visibility
+  const [bannerVisible, setBannerVisible] = useState(false);
+  const handleBannerOpen = () => setBannerVisible(true);
+  const handleBannerClose = () => setBannerVisible(false);
+
+  // On page load, check if the user has 2FA already enabled
+  // If not, check if the user has been prompted to enable 2FA (if not, show modal)
+  // Otherwise, show the banner
+  useEffect(() => {
+    const enabled = localStorage.getItem("2FA-Enabled");
+    const prompted = localStorage.getItem("promptedFor2FA");
+
+    if (!enabled) {
+      if (!prompted) {
+        handleModalOpen();
+      } else {
+        handleBannerOpen();
+      }
+    }
+  }, []);
+
+  return (
+    <body>
+      <TwoFactorAuthModal
+        visible={modalVisible}
+        handleOpen={handleModalOpen}
+        handleClose={handleModalClose}
+      />
+      <TwoFactorBanner
+        visible={bannerVisible}
+        handleClose={handleBannerClose}
+      />
+    </body>
+  );
+};
+
+export default TwoFactorAuthReminders;
