f4f0c19a69dffb0ce79de05dc73130f026f6e6c0
Darren52BT <darrenbt52@gmail.com>
2023-06-01 18:16:40 -0700

    added font size/flex box alignments for certain screen widths

---
+++ src/components/loggedOut/reviewSection.tsx
@@ -0,0 +1,118 @@
+
+import StarIcon from '@mui/icons-material/Star';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
+import StarHalfIcon from '@mui/icons-material/StarHalf';
+import React from 'react';
+import { Grid, Typography, Box, Stack } from "@mui/material";
+
+
+//contains the reviews section
+export default function reviewsSection() {
+
+    //has the title, reviews, ratings, and their corresponding reviewer names and positions
+    const title = "See What Our Users Say About Mesh"
+    const reviewMessages = ["\"Mesh has been a game-changer in my career. I found an amazing mentor who guided me through every step. Highly recommended!\"",
+        "\"As a mentor on Mesh, I have had the opportunity to make a real impact on the lives of aspiring professionals. It's an incredible platform!\"",
+        "\"Mesh is a game-changer for mentorship. It connects professionals from different backgrounds, enabling them to share their knowledge and shape the future of their industries. I'm proud to be part of this community.\"",
+        "\"I had the privilege of being a mentor on Mesh, and it has been an incredibly rewarding experience. Guiding and empowering the next generation of professionals is truly fulfilling.\" "
+
+    ]
+    const reviewers = ["- John Doe,", "- Jane Smith,", "- Rachel Patel,", "- Michael Ramirez,"];
+    const positions = ["Software Engineer", "Marketing Manager", "Business Consultant", "Financial Advisor"]
+    const numStars = [5, 4.5, 4, 5];
+
+
+    //contains array holding the rating of each review formatted into stars in a row
+    function rowStars(starNum: number) {
+        const stars = [];
+
+        //put in whole stars
+
+        let numWholeStars = Math.floor(starNum);
+
+        for (let i = 0; i < numWholeStars; i++) {
+            stars.push(<StarIcon sx={{ color: "#FFB400" }} />);
+        }
+        //put in half stars
+
+        let numHalfStars = (starNum % 1) > 0 ? 1 : 0;
+
+        if (numHalfStars > 0) {
+            stars.push(<StarHalfIcon sx={{ color: "#FFB400" }} />);
+        }
+        //put in empty stars
+        let numEmptyStars = 5 - Math.ceil(starNum);
+        for (let i = 0; i < numEmptyStars; i++) {
+            stars.push(<StarOutlineIcon sx={{ color: "#FFB400" }} />);
+        }
+
+
+
+        return stars;
+    }
+
+
+
+    //contains array of the columns of each review
+    const reviewColumns = reviewMessages.map((message, index) => {
+
+        //get the star row for the current rating
+        let stars = rowStars(numStars[index]);
+        return (
+
+            //holds the review, stacks it vertically
+            <Stack alignItems={"center"} p={5} rowGap={3} width={'30%'} sx={{
+                '@media (min-width:900px)':{
+                    width: '10%'
+                },
+                '@media (min-width: 1100px':{
+                    width: '40%'
+                }
+            
+                
+            }}>
+                {/*grey circle container for the reviewer image*/}
+                <Box width="135px" height="120px" borderRadius="50%" sx={{ background: "#D9D9D9", }}>
+                    <img src="" alt=""></img>
+                </Box>
+
+                {/*stars*/}
+                <Stack direction={"row"}>
+                    {stars}
+                </Stack>
+
+                {/*stacks the review, reviewer's name, and their position*/}
+
+                <Stack width={"100%"}>
+                    <Typography variant="body2" color="#26383A"  textAlign={'center'} >
+                        {message}
+                    </Typography>
+                    <Typography variant="body2" color="#26383A"  textAlign={'center'} >
+                        {reviewers[index]}
+                    </Typography>
+                    <Typography variant="body2" color="#26383A"  textAlign={'center'} >
+                        {positions[index]}
+                    </Typography>
+                </Stack>
+            </Stack>
+
+        )
+    })
+
+
+
+    //returns the title of the section and the review container vertically stacked on each other
+
+    return (
+
+        <Grid container item direction="column" xs={12} p={3} sx={{ background: "#F2E8DE",}}>
+
+            <Grid item xs={12}>
+                <Typography variant="h3"  textAlign={'center'}>{title}</Typography>
+                <Grid item container alignItems={"flex-start"} justifyContent={"space-evenly"} xs={12} >
+                    {reviewColumns}
+                </Grid>
+            </Grid>
+        </Grid>
+    )
+}
---
+++ src/pages/loggedOutPage.tsx
@@ -0,0 +1,217 @@
+import { Button, Grid, Link, Stack, TextField, ThemeProvider, Typography, createTheme, } from '@mui/material';
+import React from 'react';
+import "../App.css"
+
+import loggedOutNav from '../components/loggedOut/loggedOutNavBar';
+import welcomeMessage from '../components/loggedOut/loggedOutWelcome';
+import advertSection from "../components/loggedOut/advertSection"
+import reviewsSection from '../components/loggedOut/reviewSection';
+
+
+
+export default function LoggedOutPage() {
+
+    const theme = pageTheme();
+
+    return (
+
+        <ThemeProvider theme={theme} >
+            <Grid container sx={{
+                flexDirection: 'column'
+
+            }}>
+                {loggedOutNav()}
+                {/*-------------------------HEADER-------------------------*/}
+                <Grid item container direction="column" justifyContent="space-evenly" p={5} alignItems="center" xs={12} sx={{
+                    background: '#0B7D66',
+                    '@media (min-width: 600px)': {
+                        flexDirection: "row"
+                    }
+
+                }}>
+
+                    {/*-------------------------------Welcome Message--------------------------------------*/}
+
+                    {welcomeMessage()}
+
+                    {/*-------------------------------Login Bubble--------------------------------------*/}
+
+                    <Grid item container xs={8} sm={6} md={4} sx={{ background: "blue" }}>
+
+
+                        <Grid spacing={2} container item direction="column" xs>
+                            <Grid item container direction="column" spacing={5} sx={{ textAlign: 'center', alignItems: 'center' }}>
+                                <Grid item xs>
+                                    <Typography variant="h2" fontWeight={'bold'} sx={{ marginLeft: 'auto' }}>
+                                        Login
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs sx={{ width: '70%' }}>
+                                    <Stack spacing={2}>
+                                        <TextField type="text" label="Email" />
+                                        <TextField type="password" label="Password" />
+                                    </Stack>
+                                </Grid>
+                                <Grid item xs>
+                                    <Stack spacing={2}>
+                                        <Button variant="contained" sx={{ width: '15em' }}>
+                                            Login
+                                        </Button>
+                                        <Link href="#" sx={{ color: 'black', textDecoration: 'underline', fontSize: '1.5em' }}>
+                                            Forgot Password
+                                        </Link>
+                                        <Typography variant="h5" fontWeight="bold">
+                                            OR
+                                        </Typography>
+                                    </Stack>
+                                </Grid>
+                            </Grid>
+                            <Grid item xs>
+                                <Stack spacing={2} sx={{ alignItems: 'center' }}>
+                                    <Button sx={{ bgcolor: '#D9D9D9', color: 'black', '&:hover': { bgcolor: '#D9D9D9' } }}>SIGN IN WITH GOOGLE</Button>
+                                    <Button sx={{ bgcolor: '#748ADA', color: 'white', '&:hover': { bgcolor: '#748ADA' } }}>SIGN IN WITH DISCORD</Button>
+                                </Stack>
+                            </Grid>
+                        </Grid>
+
+                    </Grid>
+                </Grid>
+
+
+                {/*-----------------------Advertisement Section-------------------------------------*/}
+                {advertSection()}
+
+
+                {/*----------------------------Review Section-----------------------------------------*/}
+                {reviewsSection()}
+            </Grid>
+        </ThemeProvider>
+
+    )
+
+
+
+
+
+}
+/*theme for font sizes*/ 
+function pageTheme() {
+    const theme = createTheme();
+
+
+
+    theme.typography.h1 = {
+        fontSize: "30px", lineHeight: "30px", fontWeight: "600",
+        '@media (min-width:600px)': {
+            fontSize: '40px',
+            lineHeight: '40px'
+        },
+        '@media (min-width:900px)': {
+            fontSize: '48px',
+            lineHeight: '48px'
+        },
+        '@media (min-width: 1600px)': {
+            fontSize: '60px',
+            lineHeight: '60px'
+        }
+    }
+
+    theme.typography.h2 = {
+        fontSize: '22.5px', lineHeight: '22.5px', fontWeight: '600',
+        '@media (min-width:600px)': {
+            fontSize: '30px',
+            lineHeight: '30px'
+        },
+        '@media (min-width:900px)': {
+            fontSize: '36px',
+            lineHeight: '36px',
+
+        },
+        '@media (min-width: 1600px)': {
+            fontSize: '45px',
+            lineHeight: '45px'
+        }
+    }
+
+    theme.typography.h3 = {
+        fontSize: '20px', lineHeight: '20px', fontWeight: "600",
+        '@media (min-width:600px)': {
+            fontSize: '27px',
+            lineHeight: '27px'
+        },
+        '@media (min-width:900px)': {
+            fontSize: "30px",
+            lineHeight: "40px"
+        },
+        '@media (min-width: 1600px)': {
+            fontSize: '40px',
+            lineHeight: '40px'
+        }
+    }
+
+    theme.typography.h4 = {
+        fontSize: "18.75px", lineHeight: "25px", fontWeight: "250",
+        '@media (min-width:600px)': {
+            fontSize: '30px',
+            lineHeight: '33.5px'
+        },
+        '@media (min-width:900px)': {
+            fontSize: "30px",
+            lineHeight: "40px"
+        },
+        '@media (min-width: 1600px)': {
+            fontSize: '37.5px',
+            lineHeight: '50px'
+        }
+    }
+
+    theme.typography.body1 = {
+        fontSize: "15px", lineHeight: "20px",
+        '@media (min-width:600px)': {
+            fontSize: '20px',
+            lineHeight: '27px'
+        },
+        '@media (min-width:900px)': {
+            fontSize: "24px",
+            lineHeight: "32px",
+        },
+        '@media (min-width: 1600px)': {
+            fontSize: '30px',
+            lineHeight: '40px'
+        }
+    }
+
+    theme.typography.body2 = {
+        fontSize: "10px", lineHeight: "13px",
+        '@media (min-width:600px)': {
+            fontSize: '13.5px',
+            lineHeight: '17.5px'
+        },
+        '@media (min-width:900px)': {
+            fontSize: "16px",
+            lineHeight: "21px",
+        },
+        '@media (min-width: 1600px)': {
+            fontSize: '20px',
+            lineHeight: '26.25px'
+        }
+    }
+
+    theme.typography.button = {
+        fontSize: '12.5px', lineHeight: '15px',
+        '@media (min-width:600px)': {
+            fontSize: '16.5px',
+            lineHeight: '20px'
+        },
+        '@media (min-width:900px)': {
+            fontSize: '20px',
+            lineHeight: '24px'
+        },
+        '@media (min-width: 1600px)': {
+            fontSize: '25px',
+            lineHeight: '30px'
+        }
+    }
+
+    return theme;
+}
