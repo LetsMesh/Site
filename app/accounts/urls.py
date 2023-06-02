f1c1922901399a881e8c6483ec40a42287dff176
bluesimp1102 <jackismyname7@gmail.com>
2023-06-02 00:45:00 -0700

    Update urls.py

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -12,12 +12,3 @@ urlpatterns = [
     # PATCH  /api/accounts/:account_id
     path('<int:account_id>/', views.AccountDetailView.as_view(), name='create_account'), 
 ]
-
-# urlpatterns = [
-#     # GET    /api/accounts will returns all the current accounts
-#     # POST   /api/accounts will create accounts
-#     path('', views.default, name='default'),
-#     # GET    /api/accounts/:id will return the account with that ID if exists, 
-#     # PATCH  /api/accounts/:id will update the account with the ID (the update info will be in the request body)
-#     path('<int:account_id>/', views.with_id, name='with_id'), 
-# ]
