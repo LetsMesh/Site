b978189cb0a1d72e770a95ecc2f9738516c5ff3d
Katie <14coxkm@gmail.com>
2023-06-07 20:29:44 -0700

    DEVOPS: Major Refactor of Repository Structure. (#190)
    * DEVOPS: Refactor Repository structure. Part 1
    
    * DEVOPS: Update Readme with information regarding our project.
    
    * DEVOPS: Improve Readme after refactor.
    
    * Removed some redundancies & extra readme
    
    * DEVOPS: Refactor project structure part 2
    
    * DEVOPS: Reincluded example api
    
    * update readme slightly because of incorrect steps
    
    * cleaned up files and update readme.
    
    * add more file structures in mesh
    
    * update react readme
    
    * update backend readme with more markdown and instruction
    
    * clean up a little bit of the readme.
    
    * [DevOps] Adding meshapp clean script
    
    * DEVOPS: Refactor project part 3. Front End Files.
    
    ---------
    
    Co-authored-by: rezatnoMsirhC 🐸 <chrismontazer@gmail.com>

--- app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('/verifyemail', views.verify_email, name="verify_email")
-]
--- app/views/test_views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    if request.method == "GET":
-        print(request)
-        return HttpResponse("Hello, world.")
