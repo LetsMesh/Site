143ba4b96e9c846b9223ef7bb78344961de602a5
Leaversa <matttrv.kwong@gmail.com>
2023-06-09 01:03:44 -0700

    biography view and POST call handling

--- mesh/accounts/views.py
+++ mesh/accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
-# Create your views here.
+@csrf_exempt
+def bio_view(request):
+    if request.method == "POST":
+        data = request.POST
+        if 'biography' not in data:
+              return JsonResponse({'error': 'Invalid request. Missing biography field.'}, status=400)
+        else:
+            print(data.get('biography'))
+            # Save Biography to Database
+            return JsonResponse({'message': 'biography saved successfully'})
+    return JsonResponse({'error': 'Method not allowed'}, status=405)
