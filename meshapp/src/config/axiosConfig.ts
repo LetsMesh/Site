e7213e76475404c6f5b44d4a3368e7d09ec3695a
owwix <53359125+owwix@users.noreply.github.com>
2023-06-08 23:30:34 -0700

    fixed documentation

--- meshapp/src/config/axiosConfig.ts
+++ meshapp/src/config/axiosConfig.ts
@@ -1,15 +1,16 @@
 import axios from "axios";
 
 /*
-base URL is configured such that
-when using axios to contact django endpoint -> http://localhost:8001/api/user/reset
-set http request endpoint to -> user/reset
-to use axios, import instance as such: 
+Base URL is configured such that when you are using Axios to contact django endpoint -> 'http://localhost:8001/user/reset'
+You will need pass this request endpoint into axios call -> 'user/reset'
+
+To use axios with your form, import instance as such: 
+
 import { axiosInstance } from './config/axiosConfig'
-then you can make calls as such -> axiosInstance.get(), axiosInstance.post(), etc
+
+Then you can make calls as such -> axiosInstance.get(), axiosInstance.post(), etc
 */
 
-// const baseURL: string = process.env.NODE_ENV === 'production' ? '/api/' : 'http://localhost:8000/';
 const baseURL: string = 'http://localhost:8000/'
 
 export const axiosInstance = axios.create({
