cfcf561c5ba37fdef760c4af87be9af6feb016d5
Koki Yamaguchi <kiddiidid@gmail.com>
2023-06-12 13:15:48 -0700

    added model for message table

--- mesh/messages/models.py
+++ mesh/messages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    message_id = models.AutoField(primary_key=True)
+    from_account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='messages_from')
+    to_account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='messages_to')
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
