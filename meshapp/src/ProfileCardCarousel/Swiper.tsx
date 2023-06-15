4bc95a603d8b2513e69f50e3c75a8b2625e651ac
Darren52BT <darrenbt52@gmail.com>
2023-06-14 22:16:21 -0700

    added space styling based on window height/width, refactored and added comments

--- meshapp/src/ProfileCardCarousel/Swiper.tsx
+++ meshapp/src/ProfileCardCarousel/Swiper.tsx
@@ -1,110 +1,107 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
-import { motion } from "framer-motion";
 import { useSwipeable } from "react-swipeable";
-import {
-  Card,
-  CardMedia,
-  CardTypeMap,
-  Grid,
-  IconButton,
-  Paper,
-} from "@mui/material";
+import { Grid, IconButton } from "@mui/material";
 import profileCard from "./ProfileCard";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
-function Slider() {
-  const images = [
-    "https://media.tenor.com/cOvqBWFm4gIAAAAd/svod-thug-shaker.gif",
-    "https://media.tenor.com/59a5ljZD-uMAAAAd/cat-shut-up.gif",
-    "https://www.wideopenpets.com/wp-content/uploads/sites/6/2018/02/AdobeStock_109255057.jpg",
-    "https://images.livemint.com/img/2021/09/08/1140x641/erik-jan-leusink-IbPxGLgJiMI-unsplash_1631079336162_1631079359671.jpg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyFmpv3VDfx13aPqlY__oyW4Xpmi7VoEYo3cMcejNL9A&usqp=CAU&ec=48665698",
-  ];
-
+function Swiper() {
   const profiles = [
     {
       image: "https://media.tenor.com/cOvqBWFm4gIAAAAd/svod-thug-shaker.gif",
-      name: "THUG SHAKER",
-      job: "THE THUG SHAKER",
-      biography:
-        "BARK BARK WOOF WOOF RUFF RUFF BARK WOOF RUFF RUFF WOOF GRRR BARK BARK RUFF RUFF GRR WOOF WOOF GRRRR MEOW GRRRR MEOW MEOW MEWWWWW WOOOF WOOF GRRR RUFF MEWO MEWO",
-      interests: ["cats", "meow", "el gato", "kitty"],
+      name: "Joe Mrowgan",
+      job: "podcast host",
+      biography: "MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEEO MEW",
+      interests: ["misinformation", "cats", "meow", "el gato", "kitty"],
       details: [
-        "cat programmer in training",
+        "cat entertainer in training",
         "B.S in Milk Drinking",
         "Looking for a Mentor",
       ],
     },
     {
       image: "https://media.tenor.com/59a5ljZD-uMAAAAd/cat-shut-up.gif",
-      name: "THUG SHAKER",
-      job: "THE THUG SHAKER",
+      name: "MEOWSTER",
+      job: "milk guru",
       biography:
         "BARK BARK WOOF WOOF RUFF RUFF BARK WOOF RUFF RUFF WOOF GRRR BARK BARK RUFF RUFF GRR WOOF WOOF GRRRR MEOW GRRRR MEOW MEOW MEWWWWW WOOOF WOOF GRRR RUFF MEWO MEWO",
-      interests: ["cats", "meow", "el gato", "kitty"],
+      interests: ["wire fraud", "cats", "meow", "el gato", "kitty"],
       details: [
-        "cat programmer in training",
-        "B.S in Milk Drinking",
-        "Looking for a Mentor",
+        "cat scammer in training",
+        "B.S in Milk Laundering",
+        "Looking for a Mark",
       ],
     },
     {
       image:
         "https://www.wideopenpets.com/wp-content/uploads/sites/6/2018/02/AdobeStock_109255057.jpg",
-      name: "THUG SHAKER",
-      job: "THE THUG SHAKER",
-      biography:
-        "BARK BARK WOOF WOOF RUFF RUFF BARK WOOF RUFF RUFF WOOF GRRR BARK BARK RUFF RUFF GRR WOOF WOOF GRRRR MEOW GRRRR MEOW MEOW MEWWWWW WOOOF WOOF GRRR RUFF MEWO MEWO",
-      interests: ["cats", "meow", "el gato", "kitty"],
+      name: "Mr. Paws",
+      job: "Cat Burgular",
+      biography: "MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEEO MEW",
+      interests: ["grand larceny", "cats", "meow", "el gato", "kitty"],
       details: [
-        "cat programmer in training",
-        "B.S in Milk Drinking",
+        "cat burglar in training",
+        "B.S in Milk Robbing",
         "Looking for a Mentor",
       ],
     },
     {
       image:
         "https://images.livemint.com/img/2021/09/08/1140x641/erik-jan-leusink-IbPxGLgJiMI-unsplash_1631079336162_1631079359671.jpg",
-      name: "THUG SHAKER",
-      job: "THE THUG SHAKER",
+      name: "Mrowster Pawsident",
+      job: "Pawsident",
       biography:
         "BARK BARK WOOF WOOF RUFF RUFF BARK WOOF RUFF RUFF WOOF GRRR BARK BARK RUFF RUFF GRR WOOF WOOF GRRRR MEOW GRRRR MEOW MEOW MEWWWWW WOOOF WOOF GRRR RUFF MEWO MEWO",
-      interests: ["cats", "meow", "el gato", "kitty"],
+      interests: ["insider trading", "cats", "meow", "el gato", "kitty"],
       details: [
-        "cat programmer in training",
-        "B.S in Milk Drinking",
+        "bureaucat in training",
+        "B.S in Milk Peddling",
         "Looking for a Mentor",
       ],
     },
     {
       image:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyFmpv3VDfx13aPqlY__oyW4Xpmi7VoEYo3cMcejNL9A&usqp=CAU&ec=48665698",
-      name: "THUG SHAKER",
-      job: "THE THUG SHAKER",
-      biography:
-        "BARK BARK WOOF WOOF RUFF RUFF BARK WOOF RUFF RUFF WOOF GRRR BARK BARK RUFF RUFF GRR WOOF WOOF GRRRR MEOW GRRRR MEOW MEOW MEWWWWW WOOOF WOOF GRRR RUFF MEWO MEWO",
-      interests: ["cats", "meow", "el gato", "kitty"],
+      name: "Alex Mrownes",
+      job: "propagandist",
+      biography: "MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEOW MEEO MEW",
+      interests: ["cats", "meow", "fearmongering", "el gato", "kitty"],
       details: [
-        "cat programmer in training",
-        "B.S in Milk Drinking",
+        "cat propagandist in training",
+        "B.S in Milk Stockpiling",
         "Looking for a Mentor",
       ],
     },
   ];
+
+  //keeps track of window width
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    function setWidth() {
+      setWindowWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", setWidth);
+  });
+
+  //keeps track of the current card we're on
   const [position, positionSet] = useState(0);
 
+  //moves to the next card if possible
   const onRight = () => {
     if (position < profiles.length - 1) {
       positionSet(position + 1);
     }
   };
+
+  //moves to previous card if possible
   const onLeft = () => {
     if (position > 0) {
       positionSet(position - 1);
     }
   };
+
+  //allows mouse/touch swiping to traverse through cards
   const handlers = useSwipeable({
     delta: 0,
     onSwipedLeft: () => onRight(),
@@ -113,15 +110,27 @@ function Slider() {
     trackMouse: true,
   });
 
+  //array of all the user profile cards
   let divs = profiles.map(
     ({ image, name, job, biography, interests, details }, index: number) => {
+      //controls which card shows up on top of the stack
       let onTop: number = 0;
-      let leftPos: string = `${(index - position) * 20 + 19}vw`;
+      //controls the positioning of the cards depending on what index they are from the current position
+      //if window width is lower than 700 it will stack the cards closer together
+      let leftPos: string =
+        windowWidth > 700
+          ? `${(index - position) * 15 + 14}vw`
+          : `${(index - position) * 10 + 4.5}vw`;
+
+      //controls whether card is visible or not
       let visible: number = 1;
+
+      //if position matches index, it will be on top, otherwise it will be under
       if (position === index) {
         onTop = 1;
       } else {
         onTop = 0;
+        //if the cards aren't right before or after the current card, they will not be visible
         if (index < position - 1 || index > position + 1) {
           visible = 0;
         }
@@ -137,13 +146,15 @@ function Slider() {
         job,
         biography,
         interests,
-        details
+        details,
+        windowWidth
       );
 
       return profile;
     }
   );
 
+  //container of the card swiper, and has the arrows buttons controlling the card swiping below
   return (
     <Grid
       container
@@ -153,26 +164,23 @@ function Slider() {
       sx={{
         background: "#0B7D66",
         minHeight: "100vh",
+        padding: 0,
+        margin: 0,
         overflowX: "hidden",
       }}
     >
       <Grid
-        item
+        container
         sx={{
           position: "relative",
           width: "80vw",
-          height: "50vh",
+          height: "70vh",
         }}
         {...handlers}
       >
         {divs}
       </Grid>
-      <Grid
-        container
-        sm={10}
-        justifyContent={"space-between"}
-        sx={{ marginTop: "200px" }}
-      >
+      <Grid container xs={12} md={10} justifyContent={"space-between"}>
         <Grid item>
           <IconButton
             children={<ArrowBackIcon sx={{ fontSize: "50px" }} />}
@@ -190,4 +198,4 @@ function Slider() {
   );
 }
 
-export default Slider;
+export default Swiper;
--- meshapp/src/index.tsx
+++ meshapp/src/index.tsx
@@ -1,16 +1,15 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import TwoFactorAuthReminders from "./two-factor-auth/two-factor-reminder";
 
 import LoggedOutPage from "./home/logged-out/home";
-import Slider from './carousel/slider'
+import Slider from "./ProfileCardCarousel/Swiper";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     {/* <TwoFactorAuthReminders /> */}
-    <Slider/>
+    <Slider />
   </React.StrictMode>
 );
 
