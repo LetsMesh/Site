004b9aeee79666ca2883ae9898f08b4454d2de50
Leaversa <matttrv.kwong@gmail.com>
2023-06-15 23:08:59 -0700

    accountID check and more descriptive method error

--- mesh/profiles/views.py
+++ mesh/profiles/views.py
@@ -1,16 +1,16 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-from rest_framework.views import exception_handler
 
 @csrf_exempt
 def bio_view(request):
     if request.method == "POST":
         data = request.POST
+        if 'accountID' not in data:
+            return JsonResponse({'error': 'Invalid request. Missing accountID field.'}, status=401)
         if 'biography' not in data:
-              return JsonResponse({'error': 'Invalid request. Missing biography field.'}, status=400)
+            return JsonResponse({'error': 'Invalid request. Missing biography field.'}, status=400)
         else:
-            print(data.get('biography'))
-            # Save Biography to Database
+            print(data.get('accountID') + " " + data.get('biography')) # TODO: Save to database
             return JsonResponse({'message': 'biography saved successfully'}, status=200)
     else:
-        return JsonResponse({'error': 'Method not allowed'}, status=405)
+        return JsonResponse({'error': request.method + ' Method not allowed'}, status=405)
