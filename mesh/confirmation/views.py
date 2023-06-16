a5ec8e21082a914e4e479a448af36c887816889f
JRSiwiecki <JRSiwiecki@gmail.com>
2023-06-16 13:54:48 -0700

    Update comments, add TODO

--- mesh/confirmation/views.py
+++ mesh/confirmation/views.py
@@ -4,9 +4,16 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 import secrets
 
+# TODO: It would likely be easier to just store the 
+# unique token generated for the user in their settings,
+# and then when they click the link sent to them we can verify
+# that the one sent to them is the same as the one stored
+# in their settings.
+
+# Generate random token for user and send confirmation email
 def email_confirmation(request, user_email):
-    # Generate a token (using Django's default token generator)
-    confirmation_token = secrets.token_hex(16)  # 16 bytes converted to hex string
+    # Generate a token for the user
+    confirmation_token = secrets.token_hex(16)
 
     # Construct the confirmation URL
     confirmation_url = request.build_absolute_uri(f"/confirmation/{user_email}/{confirmation_token}/")
@@ -22,8 +29,9 @@ def email_confirmation(request, user_email):
     send_mail(email_subject, email_plain_message, 'letsmesh_testing02@outlook.com', 
               [user_email], html_message=email_message)
 
-    # Redirect the user to a success page or display a success message
     return HttpResponse(user_email + ": Confirmation email sent!")
 
+# Check that the user's stored token is the same one in the URL,
+# if so, set isVerified to true.
 def confirm_token(request, user_email, token):
     return HttpResponse(user_email + ": Email confirmation success!")
