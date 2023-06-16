53ca31ad55eef85bb5bb0164389f101a8be644e3
Leaversa <matttrv.kwong@gmail.com>
2023-06-15 22:13:14 -0700

    moved changes to correct directory

--- mesh/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views as accounts_views
-
-urlpatterns = [
-    
-    path('biography', accounts_views.bio_view)
-]
--- mesh/accounts/views.py
+++ mesh/accounts/views.py
@@ -1,16 +1,3 @@
-from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
-from rest_framework.views import exception_handler
+from django.shortcuts import render
 
-@csrf_exempt
-def bio_view(request):
-    if request.method == "POST":
-        data = request.POST
-        if 'biography' not in data:
-              return JsonResponse({'error': 'Invalid request. Missing biography field.'}, status=400)
-        else:
-            print(data.get('biography'))
-            # Save Biography to Database
-            return JsonResponse({'message': 'biography saved successfully'}, status=200)
-    else:
-        return JsonResponse({'error': 'Method not allowed'}, status=405)
+# Create your views here.
