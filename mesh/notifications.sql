002ffdf76c9ecfd2fd5bc7893233687212e456b6
William Sosropartono <wcsosropartono@gmail.com>
2023-06-19 22:01:03 -0700

    Deleted SQL table, created Model for Notification Table

--- mesh/notifications.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Notifications(
-    notificationID INT NOT NULL AUTO_INCREMENT,
-    accountID INT NOT NULL,
-    timestamp TIMESTAMP NOT NULL,
-    notificationContextJson VARCHAR(255) NOT NULL,
-    PRIMARY KEY (notificationID),
-    FOREIGN KEY (accountID) REFERENCES Accounts(accountID)
-);
