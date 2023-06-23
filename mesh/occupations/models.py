09d03ed8dc42b3168132beb112b1b6870576cb0f
shotoh <shotoh0806@gmail.com>
2023-06-22 17:05:53 -0700

    Add Occupation and OccupationBridge tables

--- mesh/occupations/models.py
+++ mesh/occupations/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
+from mesh.profiles.models import Profile
+
+
 # Create your models here.
 class Occupation(models.Model):
-    #TODO
-    pass
+    educationID = models.AutoField(primary_key=True)
+    degreeName = models.CharField(max_length=255)
+    collegeName = models.CharField(max_length=255)
+    optionalDescription = models.TextField()
+
+    def __str__(self):
+        return str(self.educationID)
+
+
 class OccupationBridge(models.Model):
-    #TODO
-    pass
+    accountID = models.OneToOneField(Profile, primary_key=True, on_delete=models.CASCADE)
+    educationID = models.OneToOneField(Occupation, on_delete=models.CASCADE)
