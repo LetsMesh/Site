a4d7e4d8c5fbe9796d263905b675a8ebf36e2b81
peppacaiou <109382836+peppacaiou@users.noreply.github.com>
2023-06-23 10:34:31 -0700

    added account checking endpoint

--- mesh/accounts/views.py
+++ mesh/accounts/views.py
@@ -3,62 +3,111 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from .models import *
 import bcrypt
-import uuid
+import os
+import dotenv
+
+# dotenv.load_dotenv()
 
 def encrypt(password ):
     salt = bcrypt.gensalt(12)
     password = password.encode('utf-8')
-    print(password)
-    print(salt)
-    return (salt,bcrypt.hashpw(password,salt))
+    
+    
+    return salt,bcrypt.hashpw(password,salt)
 
+def decrypt(password, salt):
+    password = password.encode('utf-8')
+    return bcrypt.hashpw(password,salt)
+def strongPass():
+    pass
 
 @csrf_exempt
 def account(request):
     if request.method == "POST":
         #post endpoint is creation
-        data = request.POST
-        if "password" not in data:
-            
+        body = request.POST
+        email = body["email"]
+        phone_number = body["phoneNumber"]
+        password = body["password"]
+        salt,hash = encrypt(password)
+        is_mentee = bool(body.get("isMentee",False)) 
+        is_mentor = bool(body.get("isMentor",False))
+
+        created = Account.objects.filter(email = email).values()
+        if not created:
+
+            user_account = Account(
+                email = email,
+                encryptedPass = hash,
+                salt = salt,
+                phoneNum = phone_number,
+                isMentee = is_mentee,
+                isMentor = is_mentor
+                )
+            user_account.save()
+
             return JsonResponse(
-                {"missing":"password"},
-                status = 400
+                {
+                    "status":"successfully created"
+                },
+                status = 201
             )
         else:
-
-            email = data["email"]
-            phone_number = data["phoneNumber"]
-            password = data["password"]
-            salt,hash = encrypt(password)
-            is_mentee = bool(data.get("isMentee",False)) 
-            is_mentor = bool(data.get("isMentor",False))
-
-            created = Account.objects.filter(email = email).values()
-            if not created:
-
-                user_account = Account(
-                    email = email,
-                    encryptedPass = hash,
-                    salt = salt,
-                    phoneNum = phone_number,
-                    isMentee = is_mentee,
-                    isMentor = is_mentor
-                    )
-                user_account.save()
-
-                return JsonResponse({"status":"successfully created"},status = 201)
-            else:
-                return JsonResponse(
-                    {"status":"account created already"},
-                    status = 409
-                )
+            return JsonResponse(
+                {
+                    "status":"account already exists"
+                },
+                status = 409
+            )
     else :
-        return JsonResponse({"status":f"{request.method} method not allowed"}, status = 405)
+        return JsonResponse(
+            {
+                "status":f"{request.method} method not allowed"
+            },
+            status = 405
+        )
     
 @csrf_exempt
 def checkpassword(request):
     if request.method == "POST":
-        data = request.POST
-        return JsonResponse({"here":"here"}, status = 400)
+        body = request.POST
+        email = body.get("email",False)
+        phoneNumber = body.get("phoneNumber",False)
+        password = body.get("password",False)
+        
+        user_created = Account.objects.filter(email = email,phoneNum = phoneNumber).values()
+        if not bool(user_created):
+            return JsonResponse(
+                {
+                    "error":"Account not found",  
+                },
+                status = 404
+            )
+        else:
+            hash = user_created[0]["encryptedPass"]
+            salt = user_created[0]["salt"]
+            
+            if hash == decrypt(password, salt):
+
+                return JsonResponse(
+                    {
+                        "message":"Access granted"
+                    },
+                    status = 200
+                )
+            else:
+                return JsonResponse(
+                    {
+                        "message":"Access denied, wrong password"
+                    },
+                    status = 200
+                )
+
     else :
-        return JsonResponse({"here":"here"}, status = 400)
+        return JsonResponse(
+            {
+                "status":f"{request.method} method not allowed"
+            },
+            status = 405
+        )
+    
