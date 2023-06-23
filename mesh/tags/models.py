96d30ce617e77de1a881d1f3796fa74c69e097b4
William Sosropartono <wcsosropartono@gmail.com>
2023-06-22 20:46:51 -0700

    Fixed tagBridge model

--- mesh/tags/models.py
+++ mesh/tags/models.py
@@ -8,7 +8,7 @@ class Tag(models.Model):
     isDefault = models.BooleanField()
 
 class TagBridge(models.Model):
-    class Bridge():
+    class Meta():
         unique_together = (('accountID', 'tagID'))
     tagID = models.OneToOneField(Tag, on_delete=models.CASCADE)
     accountID = models.ForeignKey(Account, on_delete=models.CASCADE)
