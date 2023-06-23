6cdf349dc7dfda66e00c1c8480815ee686cca139
peppacaiou <109382836+peppacaiou@users.noreply.github.com>
2023-06-22 23:22:26 -0700

    account creation finished

--- mesh/accounts/urls.py
+++ mesh/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from .views import *
 urlpatterns = [
-    path('createAccount',account,name = "redirect")
-    # path('checkAccount')
+    path('createAccount',account,name = "create"),
+    path('checkAccount',checkpassword, name = "check")
 ]
--- mesh/accounts/views.py
+++ mesh/accounts/views.py
@@ -18,16 +18,47 @@ def account(request):
     if request.method == "POST":
         #post endpoint is creation
         data = request.POST
-        if"password" not in data:
+        if "password" not in data:
             
-            return JsonResponse({"error":"here"}, status = 400)
+            return JsonResponse(
+                {"missing":"password"},
+                status = 400
+            )
         else:
-            
-            salt,hash = encrypt("pass")
-            print(salt)
-            print
-            return JsonResponse({"hash":"hash",
-                                 "salt":"salt"},
-                                   status = 201)
+
+            email = data["email"]
+            phone_number = data["phoneNumber"]
+            password = data["password"]
+            salt,hash = encrypt(password)
+            is_mentee = bool(data.get("isMentee",False)) 
+            is_mentor = bool(data.get("isMentor",False))
+
+            created = Account.objects.filter(email = email).values()
+            if not created:
+
+                user_account = Account(
+                    email = email,
+                    encryptedPass = hash,
+                    salt = salt,
+                    phoneNum = phone_number,
+                    isMentee = is_mentee,
+                    isMentor = is_mentor
+                    )
+                user_account.save()
+
+                return JsonResponse({"status":"successfully created"},status = 201)
+            else:
+                return JsonResponse(
+                    {"status":"account created already"},
+                    status = 409
+                )
+    else :
+        return JsonResponse({"status":f"{request.method} method not allowed"}, status = 405)
+    
+@csrf_exempt
+def checkpassword(request):
+    if request.method == "POST":
+        data = request.POST
+        return JsonResponse({"here":"here"}, status = 400)
     else :
         return JsonResponse({"here":"here"}, status = 400)
