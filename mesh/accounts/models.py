05b2d27f4c03f3402aba4651ae9a956c1a67cf35
bluesimp1102 <jackismyname7@gmail.com>
2023-06-23 17:18:16 -0700

    accounts backend, refactor w the new structure

--- mesh/accounts/models.py
+++ mesh/accounts/models.py
@@ -1,3 +1,20 @@
+# in accounts folder: (accounts.models) models.py
 from django.db import models
 
-# Create your models here.
+class Accounts(models.Model):
+    email = models.EmailField(max_length=255, unique=True)
+    encryptedPass = models.CharField(max_length=255)
+    phoneNum = models.CharField(max_length=15)
+    DISPLAY_THEMES = [
+        ('L', 'Light'),
+        ('D', 'Dark'),
+    ]
+    displayTheme = models.CharField(max_length=1, choices=DISPLAY_THEMES, default='L')
+    enabled2Factor = models.BooleanField(default=False)
+    isMentor = models.BooleanField(default=False)
+    isMentee = models.BooleanField(default=False)
+
+    class Meta:
+        app_label = 'accounts'
+        db_table = 'accounts'  # specify custom table name
+
--- mesh/settings.py
+++ mesh/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('API_SECRET_KEY')
+# SECRET_KEY = os.environ.get('API_SECRET_KEY')
+SECRET_KEY = 'asdasdasdasd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mesh',
+    'mesh.accounts'
 ]
 
 MIDDLEWARE = [
