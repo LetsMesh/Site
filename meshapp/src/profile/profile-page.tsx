dc737d5d9fd95cee7eda860c78dfcda3b7161430
Patrick Hoang <nguyenpatty42@gmail.com>
2023-06-26 19:53:18 -0700

    Leaf/pattycakesxd/f6.1 profile page picture and bio (#221)
    * implements main UI of Profile Page
    
    * fixes wordbreak issues in header+textfield UI updates
    
    * refactor occupation/bio/picture to Grid & UI fixes
    
    * fixes incorrect wrap+typography behavior
    
    * move profile-textfield.tsx to components folder
    
    * adds javadocs for all components
    
    * adds javadocs for ProfileTextField
    
    * move around files

---
+++ meshapp/src/profile/profile-page.tsx
@@ -0,0 +1,332 @@
+import React from "react";
+import {
+  Box,
+  Grid,
+  Typography,
+  createTheme,
+  ThemeProvider,
+  Chip,
+} from "@mui/material";
+
+import ProfileTextField from "./profile-textfield";
+import { Profile } from "./types/profile";
+import "./styling/profile-page.css";
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#0b7d66",
+    },
+  },
+  typography: {
+    h1: {
+      fontFamily: "cocogoose",
+      fontSize: "60px",
+      fontWeight: "bold",
+      color: "#26383A",
+    },
+    h2: {
+      fontSize: "30px",
+      fontFamily: "cocogoose",
+      color: "#26383A",
+      fontWeight: "bold",
+    },
+    h3: {
+      fontSize: "24px",
+      fontFamily: "cocogoose",
+      color: "#26383A",
+      fontWeight: "bold",
+    },
+    h4: {
+      fontSize: "22px",
+      fontFamily: "cocogoose",
+      color: "#26383A",
+      fontWeight: "bold",
+    },
+  },
+});
+
+/**
+ * A React component that renders a profile page.
+ * Displays a user's personal, professional, and educational information.
+ *
+ * @param props - Properties of the component
+ * @param {string} props.name - Name of user
+ * @param {string} props.pronouns - Pronouns used by user
+ * @param {string} props.occupationTitle - Title of user's occupation
+ * @param {string} props.occupationBusiness - Name of business/org the user affiliates with
+ * @param {string} props.biography - The initial text content of the bio
+ * @param {string} props.image - A URL to user's profile image
+ * @param {boolean} props.isMentor - Flag indicating whether the user is a mentor
+ * @param {boolean} props.isMentee - Flag indicating whether the user is a mentee
+ */
+const ProfilePage = (props: Profile) => {
+  return (
+    <Box className="profile-page-container">
+      <Box className="profile-page-header">
+        <ProfileHeader name={props.name} pronouns={props.pronouns} />
+      </Box>
+      <Grid container sx={{ borderBottom: 1, borderColor: "#d9d9d9" }}>
+        <Grid
+          item
+          xs={9}
+          sx={{
+            pl: "40px",
+          }}
+        >
+          <ProfileOccupation
+            occupationTitle={props.occupationTitle}
+            occupationBusiness={props.occupationBusiness}
+          />
+          <ProfileBiography biography={props.biography} />
+        </Grid>
+        <Grid
+          item
+          xs={3}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            pb: "15px",
+            transform: "translateY(-125px)",
+            marginBottom: "-125px", // Adjusts container height to match transform
+          }}
+        >
+          <ProfilePicture image={props.image} />
+          <ProfileRole isMentor={props.isMentor} isMentee={props.isMentee} />
+        </Grid>
+      </Grid>
+      <Box sx={{ display: "flex", flexDirection: "row", minHeight: "100vh" }}>
+        <Grid container>
+          <Grid item xs={9}>
+            <Box sx={{ borderBottom: 1, borderColor: "#d9d9d9" }}>
+              <ProfileExperience />
+            </Box>
+            <Box>
+              <ProfileEducation />
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={3}
+            sx={{
+              borderLeft: 1,
+              borderColor: "#d9d9d9",
+            }}
+          >
+            <ProfileInterests />
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
+};
+
+/**
+ * The header of the profile page.
+ *
+ * @param props - Properties of the component
+ * @param {string} props.name - Name of user
+ * @param {string} props.pronouns - Pronouns used by user
+ */
+const ProfileHeader = (props: { name: string; pronouns: string }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ pl: "40px", display: "flex", alignItems: "flex-end" }}>
+        <Grid container alignItems="flex-end">
+          <Box
+            sx={{
+              maxWidth: "1000px",
+              wordBreak: "break-word",
+            }}
+          >
+            <Typography
+              sx={{
+                lineHeight: 1,
+                display: "inline",
+              }}
+              variant="h1"
+            >
+              {props.name}
+              <Typography
+                variant="h2"
+                sx={{
+                  display: "inline",
+                }}
+              >
+                {` (${props.pronouns})`}
+              </Typography>
+            </Typography>
+          </Box>
+        </Grid>
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+/**
+ * Displays the user's profile picture.
+ *
+ * @param props - Properties of the component
+ * @param {string} props.image - A URL to user's profile image
+ */
+const ProfilePicture = (props: { image: string }) => {
+  return (
+    <Box className="profile-page-picture-container">
+      <img
+        className="profile-page-picture-body"
+        src={props.image}
+        alt="profile"
+      />
+    </Box>
+  );
+};
+
+/**
+ * Displays the user's current occupation.
+ *
+ * @param props - Properties of the component
+ * @param {string} props.occupationTitle - Title of user's occupation
+ * @param {string} props.occupationBusiness - Name of business/org the user affiliates with
+ */
+const ProfileOccupation = (props: {
+  occupationTitle: string;
+  occupationBusiness: string;
+}) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Box className="profile-page-occupations">
+        <Typography variant="h3">
+          {props.occupationTitle}, {props.occupationBusiness}
+        </Typography>
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+/**
+ * Displays the user's biography. It also has a max character limit of 300.
+ *
+ * @param props - Properties of the component
+ * @param {string} props.biography - The initial text content of the bio
+ */
+const ProfileBiography = (props: { biography: string }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Box className="profile-page-biography">
+        <ProfileTextField
+          label={"Biography"}
+          placeholder={"Share your background and experiences"}
+          text={props.biography}
+          charLimit={300}
+        />
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+/**
+ * Displays the user's role(s) (mentor, mentee, or both).
+ *
+ * @param props - Properties of the component
+ * @param {boolean} props.isMentor - Flag indicating whether the user is a mentor
+ * @param {boolean} props.isMentee - Flag indicating whether the user is a mentee
+ */
+const ProfileRole = (props: { isMentor: boolean; isMentee: boolean }) => {
+  const { isMentor, isMentee } = props;
+  let currentRole: string = isMentor
+    ? "Mentor"
+    : isMentee
+    ? "Mentee"
+    : "Unknown";
+
+  if (isMentor && isMentee) {
+    currentRole = "Mentor | Mentee";
+  }
+
+  return (
+    <Chip
+      className="profile-page-roles"
+      label={currentRole}
+      sx={{
+        color: "#26383A",
+        fontWeight: "bold",
+        fontSize: "22px",
+        fontFamily: "cocogoose",
+      }}
+      size="medium"
+    />
+  );
+};
+
+// TODO: To be fully implemented
+const ProfileExperience = (props: any) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Box className="profile-page-column-body">
+        <Typography
+          variant="h4"
+          sx={{
+            marginBottom: "20px",
+          }}
+        >
+          Experience
+        </Typography>
+        <TestComponent />
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+// TODO: To be fully implemented
+const ProfileEducation = (props: any) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Box className="profile-page-column-body">
+        <Typography
+          variant="h4"
+          sx={{
+            marginBottom: "20px",
+          }}
+        >
+          Education
+        </Typography>
+        <TestComponent />
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+// TODO: To be fully implemented
+const ProfileInterests = (props: any) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Box className="profile-page-column-body">
+        <Typography
+          variant="h4"
+          sx={{
+            marginBottom: "20px",
+          }}
+        >
+          Interests
+        </Typography>
+        <TestComponent />
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+// Temporary component as a filler for empty spaces
+const TestComponent = (props: any) => {
+  return (
+    <ProfileTextField
+      label={"Test"}
+      placeholder={"Test"}
+      text={"Test"}
+      charLimit={200}
+    />
+  );
+};
+
+export default ProfilePage;
---
+++ meshapp/src/profile/tests/profile-examples.tsx
@@ -0,0 +1,25 @@
+import { Profile } from "../types/profile";
+
+export const exampleProfile: Profile = {
+  name: "Bob Yomom",
+  pronouns: "she/her",
+  image: "https://picsum.photos/200/300",
+  isMentor: true,
+  isMentee: true,
+  occupationTitle: "Animal Petter",
+  occupationBusiness: "Self-Employed",
+  biography:
+    "Self proclaimed narcissist and professional animal petter. A vampire with many years behind me, I have been petting animals for a bagillion years.",
+};
+
+export const exampleProfile2: Profile = {
+  name: "Lysander Maximilian Bartholomew Fitzwilliam III Esquire IV",
+  pronouns: "he/he",
+  image: "https://picsum.photos/200/300",
+  isMentor: true,
+  isMentee: false,
+  occupationTitle: "Valiant Swordmaster of the Empire",
+  occupationBusiness: "Walmart",
+  biography:
+    "Trained in the art of the blade since being in the womb, became a swordmaster at the age of 3 months, and slayed approximately 4 rats throughout my perilous adventures. Also known as Michael Jackson.",
+};
