2aee04abadbaa2e3ee65b32e579d6ee0de8504d9
Katie <14coxkm@gmail.com>
2023-06-26 20:04:24 -0700

    Revert "Leaf/configure axios"

--- mesh/settings.py
+++ mesh/settings.py
@@ -49,13 +49,11 @@ INSTALLED_APPS = [
     
 ]
 
-# TODO: https://github.com/LetsMesh/Site/issues/202
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
