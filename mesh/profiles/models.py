d8e10ce51884c440b599f8cc00775da9b88e3418
JRSiwiecki <JRSiwiecki@gmail.com>
2023-06-26 20:55:00 -0700

    Replace Foreign Key accountID as OneToOne Field for Profile

--- mesh/profiles/models.py
+++ mesh/profiles/models.py
@@ -3,7 +3,7 @@ from mesh.accounts.models import Account
 # Create your models here.
 print(str(Account))
 class Profile(models.Model):
-    accountID = models.ForeignKey(Account, primary_key=True, on_delete=models.CASCADE)
+    accountID = models.OneToOneField(Account, primary_key=True, on_delete=models.CASCADE)
     userName = models.CharField(max_length= 255)
     preferredName = models.CharField(max_length= 255, null= True)
     preferredPronouns = models.CharField(max_length = 40)
