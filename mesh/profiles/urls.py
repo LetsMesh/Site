e8ec71de58cb380918fbc3404e596cdab8f9463b
William Sosropartono <wcsosropartono@gmail.com>
2023-06-27 22:02:56 -0700

    Fixed merge issue

--- mesh/profiles/urls.py
+++ mesh/profiles/urls.py
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 from django.urls import path, include
 from . import views as profile_views
 from django.http import HttpRequest
-=======
-from django.urls import path
-from . import views as profile_views
->>>>>>> dev_new
 
 urlpatterns = [
     
     path('biography', profile_views.bio_view)
-<<<<<<< HEAD
 ]
-=======
-]
->>>>>>> dev_new
