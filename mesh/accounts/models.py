4da38c1dacf305156284853569ea7f46b3acabd0
bluesimp1102 <jackismyname7@gmail.com>
2023-06-28 22:18:59 -0700

    new branch

--- mesh/accounts/models.py
+++ mesh/accounts/models.py
@@ -1,14 +1,23 @@
+# in accounts folder: (accounts.models) models.py
 from django.db import models
 
 class Account(models.Model):
     accountID = models.AutoField(primary_key=True)
-    email = models.EmailField()
+    email = models.EmailField(max_length=255, unique=True)
     encryptedPass = models.CharField(max_length=64)
-    
     phoneNum = models.CharField(max_length=15)
-    isMentee = models.BooleanField()
-    isMentor = models.BooleanField()
+    DISPLAY_THEMES = [
+        ('L', 'Light'),
+        ('D', 'Dark'),
+    ]
+    displayTheme = models.CharField(max_length=1, choices=DISPLAY_THEMES, default='L')
+    enabled2Factor = models.BooleanField(default=False)
+    isMentor = models.BooleanField(default=False)
+    isMentee = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return str(self.accountID)
     
+    class Meta:
+        app_label = 'accounts'
+        db_table = 'accounts'  # specify custom table name
--- mesh/accounts/urls.py
+++ mesh/accounts/urls.py
@@ -1,2 +1,14 @@
+# in accounts folder: urls.py  (accounts.urls)
+
+from django.contrib import admin
 from django.urls import path, include
-from .views import *
+from . import views as accounts_views
+
+urlpatterns = [
+    # GET    /api/accounts/ 
+    # POST   /api/accounts/
+    path('', accounts_views.AccountView.as_view(), name='account'),
+    # GET    /api/accounts/:account_id
+    # PATCH  /api/accounts/:account_id
+    path('<int:account_id>/', accounts_views.AccountDetailView.as_view(), name='create_account'), 
+]
