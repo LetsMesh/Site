47e1ff248d12a3f35f0829c00a8d5a75ff9b8360
Joseph <joeykahale23@gmail.com>
2023-06-29 10:12:09 -0700

    Created Settings model and blockedAccountBride

--- mesh/accountsettings/blockedaccountbridge.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS BlockedAccountBridge(
-    blockerAccountID INT,
-    blockedAccountID INT,
-
-    PRIMARY KEY (blockerAccountID, blockedAccountID),
-    FOREIGN KEY (blockerAccountID) REFERENCES Account(accountID),
-    FOREIGN KEY (blockedAccountID) REFERENCES Account(accountID)
-);
--- mesh/accountsettings/models.py
+++ mesh/accountsettings/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from mesh.accounts.models import Account
+from django.utils.translation import gettext as _
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+class Settings(models.Model):
+
+    class Themes(models.TextChoices):
+        LIGHT = '0', _('Light')
+        DARK = '1', _('Dark')
+
+    accountID = models.OneToOneField(Account, primary_key=True, on_delete=models.CASCADE)
+    isVerified = models.BooleanField(default=0)
+    verificationToken = models.CharField(max_length=35, null=True)
+    hasContentFilterEnabled = models.BooleanField(default=0)
+    displayTheme = models.CharField(choices=Themes.choices, default=Themes.LIGHT, max_length=1)
+    is2FAEnabled = models.BooleanField(default=0)
+    phoneNum = models.CharField(max_length=15, null=True)
+
+    def __str__(self) -> str:
+        return str(self.accountID)
+
+class BlockedAccountBridge(models.Model):
+
+    class Meta:
+        unique_together = ('blockerAccountID', 'blockedAccountID')
+        verbose_name_plural = 'Blocked Account Bridges'
+
+    def clean(self):
+        if self.blockerAccountID == self.blockedAccountID:
+            raise ValidationError("blockerAccountID cannot be the same as blockedAccountID.")
+
+    blockerAccountID = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='blocker')
+    blockedAccountID = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='blocked')
+
+    def __str__(self) -> str:
+        return f"self.blockerAccountID, self.blockedAccountID"
