db14267ed430f64fbd3d18c3e3008fbaa21edec1
Tony <109382836+peppacaiou@users.noreply.github.com>
2023-06-29 15:54:10 -0700

    #115  Created an endpoint to create and store an account with a bcrypt hashed password with salt and pepper #72  Created endpoint to verify if an account inputted is correct(i.e. passwords and email matched)
    * setup
    
    * fixing
    
    * created account table, and profilke table. Also added all future tables to admin
    
    * a few touchups
    
    * made a template for endpoints
    
    * altered the table and set up the wskeleton for account login
    
    * account creation finished
    
    * added account checking endpoint
    
    * implemented peppering
    
    * added migrations and made final finishing touches
    
    * fixed naming convention
    
    * made it so user cant signup use same phone number
    
    * fixed issue where isMentee will always be true
    
    * deleted sql and vscode

--- mesh/accounts/models.py
+++ mesh/accounts/models.py
@@ -3,8 +3,11 @@ from django.db import models
 class Account(models.Model):
     accountID = models.AutoField(primary_key=True)
     email = models.EmailField()
-    encryptedPass = models.CharField(max_length=64)
-    
+
+    #please set editable to False after debugging process
+    encryptedPass = models.BinaryField(max_length=64,editable = True)
+    salt = models.BinaryField(max_length = 36,editable = True) 
+    ###
     phoneNum = models.CharField(max_length=15)
     isMentee = models.BooleanField()
     isMentor = models.BooleanField()
