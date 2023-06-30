d8f9d952e6e27e9703d7f9a4fe87a3b67c88b0bc
Katie <14coxkm@gmail.com>
2023-06-30 16:32:48 -0700

    Revert "Initialized new api accountSettings, created settings and blockedaccount tables (#240)" (#242)
    This reverts commit da27d1f7988ccca7c50e4c5265ab3ac16fd02fa3.

--- mesh/settings.py
+++ mesh/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mesh.accounts',
-    'mesh.accountSettings',
     'mesh.profiles',
     'mesh.education',
     'mesh.conversation',
