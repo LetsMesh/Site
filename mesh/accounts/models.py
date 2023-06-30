598fd5aa14e19195371d50f5feb7d907620a4094
Katie <14coxkm@gmail.com>
2023-06-30 16:33:41 -0700

    Revert "Leaf/b1.2_http_post_call_handling (#238)" (#243)
    This reverts commit 2ea677367540f8b106d6aced3fc3bebeabf4f775.

--- mesh/accounts/models.py
+++ mesh/accounts/models.py
@@ -1,27 +1,17 @@
-# in accounts folder: (accounts.models) models.py
 from django.db import models
 
 class Account(models.Model):
     accountID = models.AutoField(primary_key=True)
-    email = models.EmailField(max_length=255, unique=True)
+    email = models.EmailField()
 
     #please set editable to False after debugging process
     encryptedPass = models.BinaryField(max_length=64,editable = True)
     salt = models.BinaryField(max_length = 36,editable = True) 
     ###
     phoneNum = models.CharField(max_length=15)
-    DISPLAY_THEMES = [
-        ('L', 'Light'),
-        ('D', 'Dark'),
-    ]
-    displayTheme = models.CharField(max_length=1, choices=DISPLAY_THEMES, default='L')
-    enabled2Factor = models.BooleanField(default=False)
-    isMentor = models.BooleanField(default=False)
-    isMentee = models.BooleanField(default=False)
+    isMentee = models.BooleanField()
+    isMentor = models.BooleanField()
 
     def __str__(self) -> str:
         return str(self.accountID)
     
-    class Meta:
-        app_label = 'accounts'
-        db_table = 'accounts'  # specify custom table name
--- mesh/exceptions/MissingRequiredFields.py
+++
@@ -1,8 +0,0 @@
-from .BaseCustomException import BaseCustomException
-
-class MissingRequiredFields(BaseCustomException):
-    """
-    Exception raised when one or more required fields are missing.
-    """
-    error_code = 400
-    message = 'Missing one or more required fields.'
