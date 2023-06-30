e4a653419decae48762f3c330131702b2047012c
Katie <14coxkm@gmail.com>
2023-06-30 16:26:22 -0700

    Revert "#131 G1.7 Init Notifications Table"
    This reverts commit 6ccb2daa89de0d7ac5004be23120672670efcb6d.

--- mesh/notifications/models.py
+++ mesh/notifications/models.py
@@ -1,11 +1,7 @@
 from django.db import models
-from mesh.accounts.models import Account
 
 # Create your models here.
 
 class Notification(models.Model):
-    accountID = models.ForeignKey(Account, on_delete=models.CASCADE)
-    notificationID = models.AutoField(primary_key=True)
-    timeStamp = models.TimeField()
-    notificationContextJSON = models.JSONField()
-    
+    #TODO
+    pass
