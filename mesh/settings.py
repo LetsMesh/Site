da27d1f7988ccca7c50e4c5265ab3ac16fd02fa3
Joseph Kahale <42745691+JosephKahale@users.noreply.github.com>
2023-06-30 10:39:14 -0700

    Initialized new api accountSettings, created settings and blockedaccount tables (#240)

--- mesh/settings.py
+++ mesh/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mesh.accounts',
+    'mesh.accountSettings',
     'mesh.profiles',
     'mesh.education',
     'mesh.conversation',
