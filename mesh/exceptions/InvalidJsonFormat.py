6768330b04d0e4ef949cc5e0b3b7f80bb8bd3cce
JRSiwiecki <JRSiwiecki@gmail.com>
2023-07-05 16:30:15 -0700

    Revert "* new branch"
    This reverts commit 1b0cd770f3f1bd301fca76401c4d99e179609432.

--- mesh/exceptions/InvalidJsonFormat.py
+++
@@ -1,8 +0,0 @@
-from .BaseCustomException import BaseCustomException
-
-class InvalidJsonFormat(BaseCustomException):
-    """
-    Exception raised when the JSON format is invalid.
-    """
-    error_code = 400
-    message = 'Invalid JSON format.'
--- mesh/urls.py
+++ mesh/urls.py
@@ -8,8 +8,6 @@ from mesh.profiles import urls as profile_urls
 from mesh.accounts import urls as account
 from mesh.confirmation import urls as confirmation_urls
 
-from mesh.accounts import urls as accounts_urls
-
 '''
 # How to Add URL -> Best Practices
 -----------------------------------------
