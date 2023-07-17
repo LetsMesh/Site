bf36f053f5192d4deb8dd00f5ec1d3b115286ce5
shotoh <shotoh0806@gmail.com>
2023-07-17 08:41:10 -0700

    added GET call handling for display theme

--- mesh/accountSettings/views.py
+++ mesh/accountSettings/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from mesh.accountSettings.models import Settings
+
+
+@csrf_exempt
+def display_theme(request):
+    if request.method == "GET":
+        data = request.GET
+        response = {
+            "status": "success"
+        }
+        if data.get("accountID") is None:
+            response.update({"status": "error"})
+            response.update({"message": "Missing account ID."})
+            return JsonResponse(response)
+        else:
+            if Settings.objects.filter(accountID=int(data.get("accountID"))).exists():
+                settings = Settings.objects.filter(accountID=int(data.get("accountID")))
+                response.update({
+                    "data": {
+                        "get": {
+                            "displayTheme": settings.get().displayTheme
+                        }
+                    }
+                })
+                return JsonResponse(response)
+            else:
+                response.update({"status": "error"})
+                response.update({"message": "An account does not exist with this account ID."})
+                return JsonResponse(response)
