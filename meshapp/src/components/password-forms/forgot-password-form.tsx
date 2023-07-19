c5b3c6e3d6f8629658d4ca0338e0c02453b51e1f
Leaversa <matttrv.kwong@gmail.com>
2023-07-18 20:01:37 -0700

    Aligned content

--- meshapp/src/components/password-forms/forgot-password-form.tsx
+++ meshapp/src/components/password-forms/forgot-password-form.tsx
@@ -1,78 +1,118 @@
 import { useForm } from "react-hook-form";
-import { Button, Divider, Grid, Link, Stack, Skeleton, Typography, TextField, Box } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import {
+  Button,
+  Divider,
+  Grid,
+  Link,
+  Stack,
+  Skeleton,
+  Typography,
+  TextField,
+  Box,
+} from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import React, { useState } from "react";
 import { axiosInstance } from "../../config/axiosConfig";
 
 export const PasswordReset = () => {
+  const passwordResetConfirmedEndpoint = "user/reset/confirmed";
 
-    const passwordResetConfirmedEndpoint = 'user/reset/confirmed'
+  // input validation on password reset form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    // input validation on password reset form 
-    const { register, handleSubmit, formState: { errors } } = useForm();
+  // stores password inputs in state
+  const [formData, setFormData] = useState({
+    password: "",
+    confirmedPassword: "",
+  });
 
-    // stores password inputs in state
-    const [formData, setFormData] = useState({
-        password: '',
-        confirmedPassword: '',
-
+  // updates form data in state when input fields detect change
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
     });
+  };
 
-    // updates form data in state when input fields detect change
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setFormData({
-        ...formData,
-        [e.target.name]: e.target.value,
+  // submit user data to backend
+  const onSubmit = () => {
+    if (formData.password == formData.confirmedPassword) {
+      const res = axiosInstance
+        .post(passwordResetConfirmedEndpoint, {
+          newpassword: formData.confirmedPassword,
         })
+        .then((res) => {
+          console.log(res.data);
+        });
+    } else {
+      console.log("Passwords must match!");
     }
+  };
 
-    // submit user data to backend
-    const onSubmit = () => {
-        if (formData.password == formData.confirmedPassword){
-            const res = axiosInstance.post(passwordResetConfirmedEndpoint,{
-              newpassword: formData.confirmedPassword
-          }).then(
-            (res) => {
-              console.log(res.data)
-            }
-          )
-        } else {
-          console.log("Passwords must match!")
-        }
+  // TODO: https://github.com/LetsMesh/Site/issues/255
 
-    }
-
-    // TODO: https://github.com/LetsMesh/Site/issues/255
-
-    return (
-      <Box sx={{ backgroundColor: 'primary.main', padding: '20px' }}>
-        <Grid container wrap="nowrap" alignItems="center" direction="column" spacing={5} p={2} sx={{ boxShadow: 10, margin: '20em auto', maxWidth: '480px', minWidth: '480px', bgcolor: 'secondary.main', color: 'text.primary', borderRadius: 5 }}>
-          <Grid item>
-          <Typography variant="h3" fontWeight={500} sx={{ marginLeft: 'auto', fontFamily: "cocogoose, sans-serif" }} align='center'>
+  return (
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      sx={{
+        boxShadow: 10,
+        margin: "auto",
+        width: "480px",
+        bgcolor: "secondary.main",
+        color: "text.primary",
+        borderRadius: 5,
+      }}
+    >
+      <Grid container spacing={3} p={2} width={"100%"}>
+        <Grid xs item sx={{ textAlign: "center" }}>
+          <Typography variant="h3" fontWeight={500}>
             Password Reset
           </Typography>
-          <Typography variant="h5" fontWeight={400} sx={{ marginLeft: 'auto', fontFamily: "cocogoose, sans-serif"}} align='center'>
+          <Typography variant="h5" fontWeight={400}>
             Please enter your new password
           </Typography>
         </Grid>
-        <Grid item  sx={{ width: '70%' }}>
+        <Grid item m="auto" sx={{ width: "70%" }}>
           <Stack spacing={2}>
-            <TextField type="password" label="Password" {...register("password", { required: true })} onChange={handleChange} />
+            <TextField
+              type="password"
+              label="Password"
+              {...register("password", { required: true })}
+              onChange={handleChange}
+            />
             {errors.password && <p>New password required to reset password</p>}
-            <TextField type="password" label="Confirm Password" {...register("confirmedPassword", { required: true})} onChange={handleChange} />
+            <TextField
+              type="password"
+              label="Confirm Password"
+              {...register("confirmedPassword", { required: true })}
+              onChange={handleChange}
+            />
             {errors.confirmedPassword && <p>Please re-enter new password</p>}
           </Stack>
         </Grid>
-        <Grid item>
-            <Button variant="contained" sx={{ fontSize: '1.5rem', borderRadius: 25, width: '15em', color: 'white' }} onClick={handleSubmit(onSubmit)}>
-              <Typography variant="button" >
-                Reset Password
-              </Typography>
-            </Button>
-          </Grid>
+        <Grid m={"auto"} item>
+          <Button
+            variant="contained"
+            sx={{
+              fontSize: "1.5rem",
+              borderRadius: 25,
+              width: "15em",
+              color: "white",
+            }}
+            onClick={handleSubmit(onSubmit)}
+          >
+            <Typography variant="button">Reset Password</Typography>
+          </Button>
         </Grid>
-      </Box>
-    )
-}
+      </Grid>
+    </Grid>
+  );
+};
 
-export default PasswordReset
+export default PasswordReset;
