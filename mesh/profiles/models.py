a6eabb71c1dd82ef6a0f74df452ef100aeb029fb
JRSiwiecki <JRSiwiecki@gmail.com>
2023-07-24 23:03:14 -0700

    Re-run makemigrations, re-migrate, update profile model to allow for NULL profile pictures, allow POST to specify no profile picture, and allow POST to delete profile picture.

--- mesh/profiles/models.py
+++ mesh/profiles/models.py
@@ -26,7 +26,7 @@ class Profile(models.Model):
     preferredName = models.CharField(max_length= 255, null= True)
     preferredPronouns = models.CharField(max_length = 40)
     biography = models.TextField()
-    image = models.ImageField(upload_to = path_and_rename)
+    image = models.ImageField(upload_to = path_and_rename, null = True, blank = True)
 
     def __str__(self):
         return str(self.accountID)
