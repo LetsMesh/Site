37fd58f1b706dc1fae8d334f4dee26f69acfbc6b
shotoh <75155577+shotoh@users.noreply.github.com>
2023-07-26 20:08:35 -0700

    #120: Added GET call handling for display theme
    * added GET call handling for display theme
    
    * added urls for display theme
    
    * added tests for display theme
    
    * fixed urls for display theme
    
    * removed csrf exempt
    
    * replaced filter method with get
    
    ---------
    
    Co-authored-by: shotoh <shotoh0806@gmail.com>

---
+++ mesh/tests/settings_tests.py
@@ -0,0 +1,46 @@
+import json
+
+from django.test import TestCase, Client
+
+from mesh.accountSettings.models import Settings
+from mesh.accounts.models import Account
+
+
+class SettingsTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        test_account = Account.objects.create(
+            email="settingstest@gmail.com",
+            encryptedPass=bytes("password_test", "utf-8"),
+            phoneNum="1234567890",
+            displayTheme="0",
+            enabled2Factor=False,
+            isMentor=False,
+            isMentee=True
+        )
+        Settings.objects.create(
+            accountID=test_account,
+            isVerified=False,
+            verificationToken=None,
+            hasContentFilterEnabled=False,
+            displayTheme="0",
+            is2FAEnabled=False
+        )
+
+    def test_display_theme(self):
+        test_user = Account.objects.get(email="settingstest@gmail.com")
+        response = self.client.get("/settings/displayTheme", {"accountID": test_user.accountID})
+        json_response = json.loads(response.content.decode("utf-8"))
+        self.assertEquals(json_response.get("data"), {"get": {"displayTheme": "0"}})
+
+    def test_missing_account_display_theme(self):
+        response = self.client.get("/settings/displayTheme")
+        json_response = json.loads(response.content.decode("utf-8"))
+        self.assertEquals(json_response.get("status"), "error")
+        self.assertEquals(json_response.get("message"), "Missing account ID.")
+
+    def test_no_account_display_theme(self):
+        response = self.client.get("/settings/displayTheme", {"accountID": 9999})
+        json_response = json.loads(response.content.decode("utf-8"))
+        self.assertEquals(json_response.get("status"), "error")
+        self.assertEquals(json_response.get("message"), "An account does not exist with this account ID.")
