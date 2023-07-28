ebc5e3ee12070f0ce6fb55522bc5e71e72bc6dd0
owwix <53359125+owwix@users.noreply.github.com>
2023-07-28 11:06:50 -0700

    fixed margins when resizing

--- meshapp/src/components/password-forms/initiate-reset-form.tsx
+++ meshapp/src/components/password-forms/initiate-reset-form.tsx
@@ -42,21 +42,23 @@ const ForgotPassword = (props: ComponentProps) => {
       direction="column"
       alignItems="center"
       sx={{
+        textAlign:'center',
+        width:'100%',
         margin: "auto",
         bgcolor: "cardBackground.main",
         color: "text.main",
         borderRadius: 5,
       }}
     >
-      <Grid container spacing={3} p={2} width={'100%'}>
+      <Grid container spacing={3} p={2} sx={{ width:'100%', margin:'auto' }}>
           <Grid xs sx={{ textAlign:'center' }} item>
-            <Typography variant="h2" fontWeight={'bold'} sx={{ marginLeft: 'auto', textAlign:'center' }}>
+            <Typography variant="h2" sx={{ fontWeight: 'bold', marginLeft: 'auto', textAlign:'center' }}>
               Forgot Password
             </Typography>
           </Grid>
-          <Grid xs sx={{ textAlign:'center', paddingBottom:'16px' }} item>
-            <Stack spacing={3} sx={{alignItems:'center'}}>
-              <TextField sx={{ width:'90%'}} type="text" label="Email" {...register("email", { required: true })} onChange={handleChange}/>
+          <Grid xs item>
+            <Stack spacing={3} sx={{ alignItems:'center' }}>
+              <TextField sx={{ width:'90%' }} type="text" label="Email" {...register("email", { required: true })} onChange={handleChange}/>
               <Button variant="contained" sx={{ borderRadius: 25, width: '15em' }} onClick={onClickSend}>
                 Send Link to Email
               </Button>
