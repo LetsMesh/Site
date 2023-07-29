fe2975ad105ea08152b818aba2814bca7bd4f0ad
owwix <53359125+owwix@users.noreply.github.com>
2023-07-29 12:36:27 -0700

    clean up

--- meshapp/src/components/login-form.tsx
+++ meshapp/src/components/login-form.tsx
@@ -22,10 +22,6 @@ const LoginScreen = (props: ComponentProps) => {
     console.log(formData);
   };
 
-  const forgotPasswordClicked = () => {
-    props.updateShowForgotPasswordState()
-  }
-
   return (
     <Grid
       item
