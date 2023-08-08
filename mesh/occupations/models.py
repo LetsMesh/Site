229444411ff4600d706f780be5f328c817d6a079
JRSiwiecki <JRSiwiecki@gmail.com>
2023-08-07 22:10:01 -0700

    Update Occupation and OccupationBridge models and re-make migrations

--- mesh/occupations/models.py
+++ mesh/occupations/models.py
@@ -7,8 +7,7 @@ from mesh.profiles.models import Profile
 class Occupation(models.Model):
     occupationID = models.AutoField(primary_key=True)
     occupationName = models.CharField(max_length=255)
-    occupationTag = models.CharField(max_length=255)
-    occupationDescriptor = models.TextField()
+    occupationOrganization = models.CharField(max_length=255)
 
     def __str__(self):
         return str(self.occupationID)
@@ -17,3 +16,4 @@ class Occupation(models.Model):
 class OccupationBridge(models.Model):
     accountID = models.OneToOneField(Profile, primary_key=True, on_delete=models.CASCADE)
     occupationID = models.OneToOneField(Occupation, on_delete=models.CASCADE)
+    occupationDescription = models.TextField()
