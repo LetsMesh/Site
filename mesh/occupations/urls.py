a60605b438b91376f88ce219b274beff1728fbc7
JRSiwiecki <JRSiwiecki@gmail.com>
2023-08-09 01:14:59 -0700

    Complete overhaul of HTTP handling to use Generic Django Views

--- mesh/occupations/urls.py
+++ mesh/occupations/urls.py
@@ -3,5 +3,12 @@ from . import views as occupation_views
 
 urlpatterns = [
     
-    path('', occupation_views.occupation, name="occupation")
+    # GET /occupations/
+    # POST /occupations/
+    path("", occupation_views.OccupationsView.as_view(), name="occupation"),
+
+    # GET /occupations/:occupation_id
+    # PATCH /occupations/:occupation_id
+    path("<int:account_id>/", occupation_views.OccupationsDetailView.as_view(), 
+    name="get_or_update_occupation")
 ]
--- mesh/occupations/views.py
+++ mesh/occupations/views.py
@@ -1,99 +1,164 @@
 # Django Imports
-from django.http import JsonResponse
-from django.core.exceptions import ObjectDoesNotExist
+from django.http import JsonResponse, HttpResponse
+from django.views import View
+from django.core import serializers
 
 # Model Imports
 from .models import Occupation, OccupationBridge
 from mesh.accounts.models import Account
 from mesh.profiles.models import Profile
 
+# Utilities Imports
+from ..utils.validate_data import validate_json_and_required_fields
 
-def occupation(request):
-    if request.method == "POST":
-        data = request.POST
+# Library Imports
+import json
 
-        response = {
-            "status": "Error",
-            "message": "Unable to POST occupation data."
-        }
-        
-        # Check for required fields
-        required_fields = [
-            ("accountID", "accountID not found in POST request data."),
-            ("occupationName", "occupationName not found in POST request data."),
-            ("occupationOrganization", "occupationOrganization not found in POST request data."),
-            ("occupationDescription", "occupationDescription not found in POST request data."),
-        ]
 
-        for field_name, error_message in required_fields:
-            check_result = check_required_field(data, field_name, response, error_message)
-            if check_result is not None:
-                return check_result
-        
-        account_id = data["accountID"]
+class OccupationsView(View):
+    """
+        Handles HTTP requests related to the collection of Occupation,
+        supporting GET to retrieve the list of all Occupations and
+        POST to create a new Occupation.
+    """
+    
+    def get(self, request, *args, **kwargs):
+        """
+            Handle GET requests.
+
+            Returns a JSON response containing all Occupations.
+        """
+        occupations = Occupation.objects.all()
+        occupations_list = serializers.serialize("json", occupations)
+        return JsonResponse(occupations_list, status=200)
+    
+    def post(self, request, *args, **kwargs):
+        """
+            Handle POST requests.
+
+            Creates a new occupation OR updates an existing OccupationBridge. 
+            The required fields are 
+            "accountID", "occupationName" and "occupationOrganization".
+
+            If all fields are present and valid, it returns a JSON response with
+            the newly created Occupation's ID and a HTTP status code 201,
+            indicating the Occupation has been successfully created.
+        """
+        REQUIRED_FIELDS = ["accountID", "occupationName", 
+                           "occupationOrganization", "occupationDescription"]
+
+        # Try to parse JSON input data
+        try:
+            data = validate_json_and_required_fields(request.body, REQUIRED_FIELDS)
+
+            account_id = data["accountID"]
+            occupation_name = data["occupationName"]
+            occupation_organization = data["occupationOrganization"]
+            occupation_description = data["occupationDescription"]
+
+        except (json.JSONDecodeError, KeyError):
+            return JsonResponse({"error": "Invalid JSON or missing fields."}, status=400)
         
         # Ensure account and profile exist
         try:
             account = Account.objects.get(accountID=account_id)
             profile = Profile.objects.get(accountID=account)
         
-        except ObjectDoesNotExist:
-            response["message"] = "Account or profile with that ID could not be found."
-            return JsonResponse(response, status=400)
-        
-        occupation_name = data["occupationName"]
-        occupation_organization = data["occupationOrganization"]
-        occupation_description = data["occupationDescription"]
+        except (Account.DoesNotExist, Profile.DoesNotExist):
+            return JsonResponse({"error": "Account or Profile not found."}, status=404)
         
         try:
-            # Check if occupation exists beforehand
-            occupation = Occupation.objects.get(occupationName=occupation_name, 
-                                                occupationOrganization=occupation_organization)
+            # Check if OccupationBridge already exists
+            occupation_bridge = OccupationBridge.objects.get(
+                accountID = profile
+            )
+
+            # Update existing OccupationBridge with new or existing Occupation
+            occupation = Occupation.objects.get_or_create(
+                occupationName = occupation_name,
+                occupationOrganization = occupation_organization
+            )[0]
+
+            occupation_bridge.occupationID = occupation
+            occupation_bridge.occupationDescription = occupation_description
+            occupation_bridge.save()
+
+        except OccupationBridge.DoesNotExist:
             
-            try:
-                # Check if bridge already made for user
-                occupation_bridge = OccupationBridge.objects.get(accountID=profile)
-                response["message"] = "User Occupation successfully updated."
+            # Get or create new Occupation
+            occupation = Occupation.objects.get_or_create(
+                occupationName = occupation_name,
+                occupationOrganization = occupation_organization
+            )
 
-            except ObjectDoesNotExist:
-                # Create bridge if it wasn't made before
-                occupation_bridge = OccupationBridge(accountID=profile, occupationID=occupation, 
-                                                     occupationDescription=occupation_description)
-                response["message"] = "User Occupation successfully created and linked."
-                print("3")
+            occupation_bridge = OccupationBridge.objects.create(
+                accountID = profile,
+                occupationID = occupation,
+                occupationDescription = occupation_description
+            )
+            
 
-            occupation_bridge.accountID = profile
-            occupation_bridge.occupationID = occupation
-            occupation_bridge.occupationDescription = occupation_description
-            occupation_bridge.save()
+        return JsonResponse({"occupationID": occupation.occupationID}, status=201)
+    
 
-        except ObjectDoesNotExist:
-            # Create occupation if it does not exist
-            occupation = Occupation(occupationName=occupation_name, 
-                                    occupationOrganization=occupation_organization)
-            occupation.save()
+class OccupationsDetailView(View):
+    """
+        Handles HTTP requests related to a singular Occupation,
+        supporting GET to retrieve one Occupation, or PATCH 
+        to update an existing OccupationBridge.
+    """
 
-            try:
-                # Check if bridge already made for user
-                occupation_bridge = OccupationBridge.objects.get(accountID=profile)
-                response["message"] = "User Occupation successfully created and linked."
+    def get(self, request, account_id, *args, **kwargs):
+        """
+            Handle GET requests for a single Account's Occupation.
 
-            except ObjectDoesNotExist:
-                # Create bridge if it wasn't made before
-                occupation_bridge = OccupationBridge(accountID=profile, occupationID=occupation, 
-                                                     occupationDescription=occupation_description)
-                response["message"] = "User Occupation successfully created and linked."
+            Returns a JSON response containing the requested Occupation.
 
-            occupation_bridge.accountID = profile
-            occupation_bridge.occupationID = occupation
-            occupation_bridge.occupationDescription = occupation_description
-            occupation_bridge.save()
-
-        response["status"] = "Success"
-        return JsonResponse(response, status=200)
+            If the Occupation does not exist, it returns a 404 status code
+            and an error message.
+        """
         
-def check_required_field(data, field_name, response, field_error_message):
-    if field_name not in data:
-        response["message"] = field_error_message
-        return JsonResponse(response, status=400)
-    return None
+        try:
+            occupation_bridge = OccupationBridge.objects.get(accountID_id=account_id)
+            
+            occupation = occupation_bridge.occupationID
+            occupation_data = {
+                "occupationID": occupation.occupationID,
+                "occupationName": occupation.occupationName,
+                "occupationOrganization": occupation.occupationOrganization,
+                "occupationDescription": occupation_bridge.occupationDescription,
+            }
+            
+            return JsonResponse(occupation_data, status=200)
+        
+        except OccupationBridge.DoesNotExist:
+            return JsonResponse({"message": "OccupationBridge not found"}, status=404)
+    
+    def patch(self, request, account_id, *args, **kwargs):
+        """
+            Handle PATCH requests for a single Occupation.
+
+            Updates the specified fields (currently, only the description) 
+            of the OccupationBridge of the given accountID. 
+
+            If the OccupationBridge does not exist, it returns a 404 status code and
+            and error message.
+
+            If a field is not provided in the request body, the current value for that field
+            will remain unchanged.
+
+            Returns a 204 HTTP status to indicate that the request has
+            succeeded butt does not include an entity-body in the response. 
+        """
+        try:
+            occupation_bridge = OccupationBridge.objects.get(accountID=account_id)
+        
+        except Occupation.DoesNotExist:
+            return JsonResponse({"error": "OccupationBridge does not exist."}, status=404)
+        
+        data = json.loads(request.body)
+
+        occupation_bridge.occupationDescription = data.get("occupationDescription", 
+                                                           occupation_bridge.occupationDescription)
+        occupation_bridge.save()
+        return HttpResponse(status=204)
