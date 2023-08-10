e54d1d7f94576120364edde4dc4f27dd234b39ab
tdang2180 <114024874+tdang2180@users.noreply.github.com>
2023-08-09 17:09:01 -0700

    added views to handle get requests

--- mesh/accountSettings/views.py
+++ mesh/accountSettings/views.py
@@ -1,8 +1,8 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
-
+from django.views import View
+from django.core import serializers
 from mesh.accountSettings.models import Settings
-
 
 def display_theme(request):
     if request.method == "GET":
@@ -29,3 +29,25 @@ def display_theme(request):
                 response.update({"status": "error"})
                 response.update({"message": "An account does not exist with this account ID."})
                 return JsonResponse(response)
+
+class SettingsView(View):
+    def get(self, request, *args, **kwargs):
+        """
+        Handles GET requests when the client fetches for all accounts settings
+        """
+        settings = Settings.objects.all()
+        accounts_settings = serializers.serialize('json', settings)
+        return JsonResponse(accounts_settings, safe=False)
+
+class SettingsDetailView(View):
+    def get(self, request, account_id, *args, **kwargs):
+        """
+        Handles GET requests when the client fetches for a specific account settings
+        """
+        try: 
+            settings = Settings.objects.get(accountID= account_id)
+            settings_detail = serializers.serialize('json', [settings])
+            return JsonResponse(settings_detail, safe = False)
+        
+        except Settings.DoesNotExist:
+            return JsonResponse({'error': 'Settings for this account does not exist'}, status=404)
