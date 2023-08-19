f44b2762246ed434d8d9eda51939a9f687df9772
JRSiwiecki <JRSiwiecki@gmail.com>
2023-08-19 00:14:22 -0700

    Update url comments

--- mesh/profiles/urls.py
+++ mesh/profiles/urls.py
@@ -8,18 +8,19 @@ urlpatterns = [
     # PATCH /profiles/profile-picture
     path("profile-picture", profile_views.ProfilePicturesView.as_view(), name="profile-picture"),
     
-    # GET    /api/biography/:account_id
-    # POST   /api/biography/:account_id
+    # GET    /profiles/biography/:account_id
+    # POST   /profiless/biography/:account_id
     path('biography/<int:account_id>/', profile_views.BiographyView.as_view(), name='biography'),
 
     # GET /profiles/profile-picture/:account_id
     path("profile-picture/<int:account_id>", profile_views.ProfilePictureDetailsView.as_view(), name="profile-picture-details"),
 
+    # GET /profiles/user-name/:account_id
     path("user-name/<int:account_id>", profile_views.UserNamesView.as_view(), name="user-name"),
     
-    # GET /profiles/profile-picture/:account_id
+    # GET /profiles/preferred-name/:account_id
     path("preferred-name/<int:account_id>", profile_views.PreferredNamesView.as_view(), name="preferred-name"),
     
-    # GET /profiles/profile-picture/:account_id
+    # GET /profiles/preferred-pronouns/:account_id
     path("preferred-pronouns/<int:account_id>", profile_views.PreferredPronounsView.as_view(), name="preferred-pronouns"),
 ]
