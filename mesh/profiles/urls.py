2fcb498a8e6256e2683d7c73ecf808948f366fd9
Joseph <46443751+JRSiwiecki@users.noreply.github.com>
2023-08-18 20:58:53 -0700

    Remove path to non-existent function (#286)

--- mesh/profiles/urls.py
+++ mesh/profiles/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     # GET    /api/biography/:account_id
     # POST   /api/biography/:account_id
     path('biography/<int:account_id>/', profile_views.BiographyView.as_view(), name='biography'),
-    path("profilePicture", profile_views.profile_picture, name="profile_picture"),
     path("profile-picture/<int:account_id>", profile_views.ProfilePicturesView.as_view(), name="profile-picture"),
     path("user-name/<int:account_id>", profile_views.UserNamesView.as_view(), name="user-name"),
     path("preferred-name/<int:account_id>", profile_views.PreferredNamesView.as_view(), name="preferred-name"),
