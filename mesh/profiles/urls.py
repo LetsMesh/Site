87807a56462b0aefe9de7baa34d469662f6438fb
JRSiwiecki <JRSiwiecki@gmail.com>
2023-08-18 19:31:33 -0700

    Update comments, formatting

--- mesh/profiles/urls.py
+++ mesh/profiles/urls.py
@@ -2,14 +2,25 @@ from django.urls import path, include
 from . import views as profile_views
 
 urlpatterns = [
-    path('biography', profile_views.bio_view),
-
-    path("profile-picture", profile_views.ProfilePicturesView.as_view(), name="profile-picture")
+    
+    # GET /profiles/profile-picture
+    # POST /profiles/profile-picture
+    # PATCH /profiles/profile-picture
+    path("profile-picture", profile_views.ProfilePicturesView.as_view(), name="profile-picture"),
+    
     # GET    /api/biography/:account_id
     # POST   /api/biography/:account_id
     path('biography/<int:account_id>/', profile_views.BiographyView.as_view(), name='biography'),
-    path("profile-picture/<int:account_id>", profile_views.ProfilePicturesView.as_view(), name="profile-picture"),
+    
+    # GET /profiles/profile-picture/:account_id
+    path("profile-picture/<int:account_id>", profile_views.ProfilePictureDetailsView.as_view(), name="profile-picture-details"),
+    
+    # GET /profiles/profile-picture/:account_id
     path("user-name/<int:account_id>", profile_views.UserNamesView.as_view(), name="user-name"),
+    
+    # GET /profiles/profile-picture/:account_id
     path("preferred-name/<int:account_id>", profile_views.PreferredNamesView.as_view(), name="preferred-name"),
+    
+    # GET /profiles/profile-picture/:account_id
     path("preferred-pronouns/<int:account_id>", profile_views.PreferredPronounsView.as_view(), name="preferred-pronouns"),
 ]
--- mesh/profiles/views.py
+++ mesh/profiles/views.py
@@ -14,7 +14,7 @@ from mesh.utils.validate_data import validate_json_and_required_fields
 from mesh.exceptions.MissingRequiredFields import MissingRequiredFields
 from mesh.exceptions.InvalidJsonFormat import InvalidJsonFormat
 
-# Library Imports 
+# Library Imports
 import os
 import uuid
 import json
@@ -23,10 +23,12 @@ from urllib.parse import urlparse
 from requests.exceptions import HTTPError
 from b2sdk.v1 import B2Api, InMemoryAccountInfo, UploadSourceBytes
 
+
 class BiographyView(View):
     """
     View for getting an biography by accountId or updating an biography by accountID.
     """
+
     def get(self, request, account_id, *args, **kwargs):
         """
         Handle GET requests.
@@ -35,87 +37,105 @@ class BiographyView(View):
 
         Returns a JSON response containing biography of specified profile through id.
         """
-        try:     
+        try:
             profile = Profile.objects.get(accountID=account_id)
             profile_biography = profile.biography
-            return JsonResponse({'message': profile_biography}, safe=False, status=200)
+            return JsonResponse({"message": profile_biography}, safe=False, status=200)
         except Account.DoesNotExist:
-            return JsonResponse({'error': 'Invalid request. Account does not exist'}, status=404)
+            return JsonResponse(
+                {"error": "Invalid request. Account does not exist"}, status=404
+            )
         except Profile.DoesNotExist:
-            return JsonResponse({'error': 'Invalid request. Profile does not exist'}, status=404)
+            return JsonResponse(
+                {"error": "Invalid request. Profile does not exist"}, status=404
+            )
 
     def post(self, request, *args, **kwargs):
         if request.method == "POST":
             data = request.POST
-            if 'accountID' not in data:
-                return JsonResponse({'error': 'Invalid request. Missing accountID field.'}, status=401)
-            if 'biography' not in data:
-                return JsonResponse({'error': 'Invalid request. Missing biography field.'}, status=400)
+            if "accountID" not in data:
+                return JsonResponse(
+                    {"error": "Invalid request. Missing accountID field."}, status=401
+                )
+            if "biography" not in data:
+                return JsonResponse(
+                    {"error": "Invalid request. Missing biography field."}, status=400
+                )
             else:
-                print(data.get('accountID') + " " + data.get('biography'))  # TODO: Save to database
-                return JsonResponse({'message': 'biography saved successfully'}, status=200)
+                print(
+                    data.get("accountID") + " " + data.get("biography")
+                )  # TODO: Save to database
+                return JsonResponse(
+                    {"message": "biography saved successfully"}, status=200
+                )
         else:
-            return JsonResponse({'error': request.method + ' Method not allowed'}, status=405)     
+            return JsonResponse(
+                {"error": request.method + " Method not allowed"}, status=405
+            )
+
 
 class ProfilePicturesView(View):
-     """
-        Handles HTTP requests related to Profile Pictures, supporting
-        GET to retrieve a profile picture,
-        POST to upload a profile picture,
-        PATCH to update a profile picture,
-        DELETE to delete a profile picture.
     """
-    
-    def get(self, request, account_id, *args, **kwargs):
-        return get_data(account_id, "profilePicture", lambda profile: profile.profilePicture)
-      
+    Handles HTTP requests related to Profile Pictures, supporting
+    POST to upload a profile picture,
+    PATCH to update a profile picture,
+    DELETE to delete a profile picture.
+    """
+
     def post(self, request, *args, **kwargs):
         """
-            Handles POST requests.
+        Handles POST requests.
 
-            Grabs an existing Profile and uploads a profile picture to imgur,
-            and saves the link with the Profile.
+        Grabs an existing Profile and uploads a profile picture to imgur,
+        and saves the link with the Profile.
 
-            The required fields are "accountID" and "profilePicture".
+        The required fields are "accountID" and "profilePicture".
 
-            Note: A user should not have a profile picture by this point, as in their
-            profilePicture URL should be empty. If they already have a picture, 
-            a PATCH request should be made instead. If a user wishes to delete their picture,
-            a DELETE request should be made instead.
+        Note: A user should not have a profile picture by this point, as in their
+        profilePicture URL should be empty. If they already have a picture,
+        a PATCH request should be made instead. If a user wishes to delete their picture,
+        a DELETE request should be made instead.
         """
-        
         try:
             data = request.POST
 
             account_id = data["accountID"]
-            
+
             profile = Profile.objects.get(accountID=account_id)
-            
+
             # Check if user already has a profilePicture
             if profile.profilePicture is not None:
-                return JsonResponse({"error": "Profile already has a profilePicture. " +
-                                     "Use PATCH to update the profilePicture or " + 
-                                     "DELETE to delete the profilePicture."}, 
-                                    status = 409)
-            
+                return JsonResponse(
+                    {
+                        "error": "Profile already has a profilePicture. "
+                        + "Use PATCH to update the profilePicture or "
+                        + "DELETE to delete the profilePicture."
+                    },
+                    status=409,
+                )
+
             backblaze_api, backblaze_bucket = initialize_backblaze_client()
-            
+
             # Grab photo from request files
             image_file = request.FILES["profilePicture"]
 
             if check_if_file_is_not_image(image_file):
-                return JsonResponse({"error": "Uplodaed file is not an image."}, status = 400)
+                return JsonResponse(
+                    {"error": "Uplodaed file is not an image."}, status=400
+                )
 
             image_file = image_file.read()
-            
+
             original_file_name = request.FILES["profilePicture"].name
-            
+
             # rename photo to random string + accountID
             new_file_name = generate_unique_filename(account_id, original_file_name)
-            
+
             # upload image to backblaze
             upload_source = UploadSourceBytes(image_file)
-            upload_result = backblaze_bucket.upload(upload_source,  file_name=new_file_name)
+            upload_result = backblaze_bucket.upload(
+                upload_source, file_name=new_file_name
+            )
 
             image_link = generate_image_url(new_file_name)
 
@@ -123,51 +143,62 @@ class ProfilePicturesView(View):
             profile.profilePicture = image_link
             profile.save()
 
-            request_response = json.dumps({"profileID": profile.accountID.accountID ,
-                                 "profilePicture": image_link})
-            
-            return JsonResponse(request_response, status = 201, safe = False)
+            request_response = json.dumps(
+                {"profileID": profile.accountID.accountID, "profilePicture": image_link}
+            )
+
+            return JsonResponse(request_response, status=201, safe=False)
 
         except MultiValueDictKeyError:
-            return JsonResponse({"error": "Missing required JSON fields."}, status = 400)
+            return JsonResponse({"error": "Missing required JSON fields."}, status=400)
 
         except Profile.DoesNotExist:
-            return JsonResponse({"error": "Profile not found."}, status = 404)
-        
+            return JsonResponse({"error": "Profile not found."}, status=404)
+
         except HTTPError:
-            return JsonResponse({"error": "Uploaded file is not an image."}, status = 400)
+            return JsonResponse({"error": "Uploaded file is not an image."}, status=400)
 
         except ValueError:
-            return JsonResponse({"error": "accountID or profilePicture field is empty."}, status = 400)
+            return JsonResponse(
+                {"error": "accountID or profilePicture field is empty."}, status=400
+            )
 
     def patch(self, request, *args, **kwargs):
-
         try:
-            data = MultiPartParser(request.META, request, request.upload_handlers).parse()
-            
+            data = MultiPartParser(
+                request.META, request, request.upload_handlers
+            ).parse()
+
             account_id = data[0]["accountID"]
 
             profile = Profile.objects.get(accountID=account_id)
 
             if profile.profilePicture is None:
-                return JsonResponse({"error": "Profile does not have an existing profilePicture." +
-                                     "Use a POST request to upload a profilePicture."}, status = 409)
+                return JsonResponse(
+                    {
+                        "error": "Profile does not have an existing profilePicture."
+                        + "Use a POST request to upload a profilePicture."
+                    },
+                    status=409,
+                )
 
             # need to delete old profile picture first,
             # then upload new profile picture
 
             # Grab photo from request files
             image_file = data[1]["profilePicture"]
-            
+
             if check_if_file_is_not_image(image_file):
-                return JsonResponse({"error": "Uplodaed file is not an image."}, status = 400)
-            
+                return JsonResponse(
+                    {"error": "Uplodaed file is not an image."}, status=400
+                )
+
             image_file = image_file.read()
-            
+
             # parse url, get file name
             profile_picture_url = urlparse(profile.profilePicture)
             file_name = os.path.basename(profile_picture_url.path)
-            
+
             # find file info to grab file id
             backblaze_api, backblaze_bucket = initialize_backblaze_client()
             file_version_info = backblaze_bucket.get_file_info_by_name(file_name)
@@ -175,67 +206,72 @@ class ProfilePicturesView(View):
 
             # delete old profile picture
             backblaze_bucket.delete_file_version(file_id, file_name)
-            
+
             # rename photo to random string + accountID
             original_file_name = data[1]["profilePicture"].name
             new_file_name = generate_unique_filename(account_id, original_file_name)
-            
+
             # upload image to backblaze
             upload_source = UploadSourceBytes(image_file)
-            upload_result = backblaze_bucket.upload(upload_source,  file_name=new_file_name)
+            upload_result = backblaze_bucket.upload(
+                upload_source, file_name=new_file_name
+            )
 
             image_link = generate_image_url(new_file_name)
-            
+
             # Save photo URL with user
             profile.profilePicture = image_link
             profile.save()
 
-            request_response = json.dumps({"profileID": profile.accountID.accountID ,
-                                 "profilePicture": image_link})
-            
-            return JsonResponse(request_response, status = 200, safe = False)
-        
+            request_response = json.dumps(
+                {"profileID": profile.accountID.accountID, "profilePicture": image_link}
+            )
+
+            return JsonResponse(request_response, status=200, safe=False)
+
         except MultiValueDictKeyError:
-            return JsonResponse({"error": "Missing required JSON fields."}, status = 400)
-        
+            return JsonResponse({"error": "Missing required JSON fields."}, status=400)
+
         except Profile.DoesNotExist:
-            return JsonResponse({"error": "Profile not found."}, status = 404)
-        
+            return JsonResponse({"error": "Profile not found."}, status=404)
+
         except HTTPError:
-            return JsonResponse({"error": "Uploaded file is not an image."}, status = 400)
-        
+            return JsonResponse({"error": "Uploaded file is not an image."}, status=400)
+
         except ValueError:
-            return JsonResponse({"error": "accountID or profilePicture field is empty."}, status = 400)
-
-
+            return JsonResponse(
+                {"error": "accountID or profilePicture field is empty."}, status=400
+            )
 
     def delete(self, request, *args, **kwargs):
         """
-            Handles DELETE requests.
+        Handles DELETE requests.
 
-            Grabs an existing Profile and deletes its profilePicture.
+        Grabs an existing Profile and deletes its profilePicture.
 
-            The required field is "accountID".
+        The required field is "accountID".
         """
-        
+
         REQUIRED_FIELDS = ["accountID"]
-        
+
         try:
             data = validate_json_and_required_fields(request.body, REQUIRED_FIELDS)
 
             account_id = data["accountID"]
-            
+
             profile = Profile.objects.get(accountID=account_id)
 
             # Ensure their profilePicture does not exist already.
             if profile.profilePicture is None:
-                return JsonResponse({"error": "Profile's profilePicture not found, nothing to delete."},
-                                    status = 404)
-            
+                return JsonResponse(
+                    {"error": "Profile's profilePicture not found, nothing to delete."},
+                    status=404,
+                )
+
             # parse url, get file name
             profile_picture_url = urlparse(profile.profilePicture)
             file_name = os.path.basename(profile_picture_url.path)
-            
+
             # find file info to grab file id
             backblaze_api, backblaze_bucket = initialize_backblaze_client()
             file_version_info = backblaze_bucket.get_file_info_by_name(file_name)
@@ -251,17 +287,23 @@ class ProfilePicturesView(View):
             return HttpResponse(status=204)
 
         except InvalidJsonFormat:
-            return JsonResponse({"error": "Invalid JSON format."}, status = 400)
+            return JsonResponse({"error": "Invalid JSON format."}, status=400)
 
         except MissingRequiredFields:
-            return JsonResponse({"error": "Missing required JSON fields."}, status = 400)
+            return JsonResponse({"error": "Missing required JSON fields."}, status=400)
 
         except Profile.DoesNotExist:
-            return JsonResponse({"error": "Profile not found."}, status = 404)
-        
-        except ValueError:
-            return JsonResponse({"error": "accountID or profilePicture field is empty."}, status = 400)
+            return JsonResponse({"error": "Profile not found."}, status=404)
 
+        except ValueError:
+            return JsonResponse(
+                {"error": "accountID or profilePicture field is empty."}, status=400
+            )
+
+
+class ProfilePictureDetailsView(View):
+    def get(self, request, account_id, *args, **kwargs):
+        return get_data(account_id, "profilePicture", lambda profile: profile.profilePicture)
 
 class UserNamesView(View):
     def get(self, request, account_id, *args, **kwargs):
@@ -270,30 +312,33 @@ class UserNamesView(View):
 
 class PreferredNamesView(View):
     def get(self, request, account_id, *args, **kwargs):
-        return get_data(account_id, "preferredName", lambda profile: profile.preferredName)
+        return get_data(
+            account_id, "preferredName", lambda profile: profile.preferredName
+        )
 
 
 class PreferredPronounsView(View):
     def get(self, request, account_id, *args, **kwargs):
-        return get_data(account_id, "preferredPronouns", lambda profile: profile.preferredPronouns)
+        return get_data(
+            account_id, "preferredPronouns", lambda profile: profile.preferredPronouns
+        )
 
 
 def get_data(account_id, name, mapper):
     try:
         profile = Profile.objects.get(accountID=int(account_id))
-        return JsonResponse({
-            "status": "success",
-            "data": {
-                "get": {
-                    name: mapper(profile)
-                }
-            }
-        }, status=200)
+        return JsonResponse(
+            {"status": "success", "data": {"get": {name: mapper(profile)}}}, status=200
+        )
     except ObjectDoesNotExist:
-        return JsonResponse({
-            "status": "error",
-            "message": "An account does not exist with this account ID."
-        }, status=404)
+        return JsonResponse(
+            {
+                "status": "error",
+                "message": "An account does not exist with this account ID.",
+            },
+            status=404,
+        )
+
 
 def initialize_backblaze_client():
     BACKBLAZE_APPLICATION_KEY_ID = os.environ.get("BACKBLAZE_MASTER_KEY")
@@ -302,10 +347,13 @@ def initialize_backblaze_client():
 
     account_info = InMemoryAccountInfo()
     backblaze_api = B2Api(account_info)
-    backblaze_api.authorize_account("production", BACKBLAZE_APPLICATION_KEY_ID, BACKBLAZE_APPLICATION_KEY)
+    backblaze_api.authorize_account(
+        "production", BACKBLAZE_APPLICATION_KEY_ID, BACKBLAZE_APPLICATION_KEY
+    )
     bucket = backblaze_api.get_bucket_by_name(BACKBLAZE_BUCKET_NAME)
 
     return backblaze_api, bucket
+
 
 # create unique, random file name containing accountID + random string
 def generate_unique_filename(account_id, original_filename):
@@ -321,13 +369,13 @@ def generate_image_url(file_name):
     file_url = BACKBLAZE_URL + file_name
     return file_url
 
+
 # ensure uploaded file is an image
 def check_if_file_is_not_image(file):
-    
     ACCEPTED_FILE_TYPES = ["jpeg", "jpg", "png"]
     file_type = imghdr.what(file)
 
     if file_type not in ACCEPTED_FILE_TYPES:
         return True
-    
+
     return False
