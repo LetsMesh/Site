478e5734e94c60304616654ed99390689e065765
BradJKim <bradjkim03@gmail.com>
2023-08-20 20:16:33 -0700

    url update for 2factauth, implemented class-based request handling and set up get request

--- mesh/accountSettings/urls.py
+++ mesh/accountSettings/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views as accountsettings_views
 
 urlpatterns = [
-    path("displayTheme", accountsettings_views.display_theme, name="display_theme")
+    path("displayTheme", accountsettings_views.display_theme, name="display_theme"),
+    path("twoFactAuth", accountsettings_views.two_fact_auth, name="two_fact_auth")
 ]
--- mesh/accountSettings/views.py
+++ mesh/accountSettings/views.py
@@ -1,5 +1,7 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
+from django.views import View
+from django.core import serializers
 
 from mesh.accountSettings.models import Settings
 
@@ -29,3 +31,32 @@ def display_theme(request):
                 response.update({"status": "error"})
                 response.update({"message": "An account does not exist with this account ID."})
                 return JsonResponse(response)
+
+class TwoFactAuthView(View):
+    """ 
+    Handles HTTP requests related to Two Factor Authentication account settings.
+    Support Get request to retrieve setting and patch to change setting.
+    """
+    def get(self, request, account_id, *args, **kwargs):
+        """ 
+        Handles GET request
+
+        Return JSON with Two Factor Authentication Settings for given account ID. 
+        Returns a 404 error with error message if account does not exist.
+        """
+        try:
+            settings_2FactAuth = Settings.objects.get(accountID=account_id).is2FAEnabled
+            settings_detail = serializers.serialize('json', [settings_2FactAuth])
+            return JsonResponse(settings_detail, safe=False)
+        except ObjectDoesNotExist:
+            return JsonResponse({'error': 'An account does not exist with this account ID.'}, status=404)
+
+    
+    def patch():
+        """ 
+        Handles Patch request
+        
+        t
+        """
+        
+
