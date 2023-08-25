e42b46143502e9f4cb2d4b8d436a5a4b30068ddf
tdang2180 <114024874+tdang2180@users.noreply.github.com>
2023-08-24 23:08:14 -0700

    added comments for post requests

--- mesh/profiles/urls.py
+++ mesh/profiles/urls.py
@@ -9,19 +9,22 @@ urlpatterns = [
     # DELETE /profiles/profile-picture
     path("profile-picture", profile_views.ProfilePicturesView.as_view(), name="profile-picture"),
     
-    # GET    /profiles/biography/:account_id
-    # POST   /profiless/biography/:account_id
+    # GET /profiles/biography/:account_id
+    # POST /profiles/biography/:account_id
     path('biography/<int:account_id>/', profile_views.BiographyView.as_view(), name='biography'),
 
     # GET /profiles/profile-picture/:account_id
     path("profile-picture/<int:account_id>", profile_views.ProfilePictureDetailsView.as_view(), name="profile-picture-details"),
 
     # GET /profiles/user-name/:account_id
+    # POST /profiles/user-name/:account_id
     path("user-name/<int:account_id>", profile_views.UserNamesView.as_view(), name="user-name"),
     
     # GET /profiles/preferred-name/:account_id
+    # POST /profiles/preferred-name/:account_id
     path("preferred-name/<int:account_id>", profile_views.PreferredNamesView.as_view(), name="preferred-name"),
     
     # GET /profiles/preferred-pronouns/:account_id
+    # POST /profiles/preferred-pronouns/:account_id
     path("preferred-pronouns/<int:account_id>", profile_views.PreferredPronounsView.as_view(), name="preferred-pronouns"),
 ]
