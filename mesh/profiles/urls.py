6d990594e3538d105559f84b42bd710dd1fab2e3
Isaac Alaniz <isaacalaniz85@gmail.com>
2023-08-30 13:42:26 -0700

    Removed duplicate views

--- mesh/profiles/urls.py
+++ mesh/profiles/urls.py
@@ -19,8 +19,6 @@ urlpatterns = [
 
     # GET /profiles/user-name/:account_id
     # POST /profiles/user-name/:account_id
-    path("biography/<int:account_id>", profile_views.BiographyView.as_view()),
-    path("profile-picture/<int:account_id>", profile_views.ProfilePicturesView.as_view(), name="profile-picture"),
     path("user-name/<int:account_id>", profile_views.UserNamesView.as_view(), name="user-name"),
     
     # GET /profiles/preferred-name/:account_id
