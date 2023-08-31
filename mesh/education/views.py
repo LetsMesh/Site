4652a889fa2a915012a257082b21789e74cb8033
Ian Dai <ianbbdai@gmail.com>
2023-08-31 00:50:37 -0700

    Connected url to view, post method runs
    Also created more tests for failures

--- mesh/education/views.py
+++ mesh/education/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# Django Imports
+from django.http import JsonResponse, HttpResponse
+from django.views import View
+from django.core import serializers
+from django.utils.datastructures import MultiValueDictKeyError
 
-# Create your views here.
+# Model Imports
+from .models import Education, EducationBridge
+from mesh.accounts.models import Account
+from mesh.profiles.models import Profile
+
+# Utilities Imports
+from mesh.utils.validate_data import validate_json_and_required_fields
+from mesh.exceptions.MissingRequiredFields import MissingRequiredFields
+from mesh.exceptions.InvalidJsonFormat import InvalidJsonFormat
+
+# Library Imports
+import json
+
+
+class EducationView(View):
+    def post(self, request, *args, **kwargs):
+        return HttpResponse(status=200)
--- mesh/tests/education_tests.py
+++ mesh/tests/education_tests.py
@@ -6,28 +6,82 @@ from ..education.models import EducationBridge
 
 
 class EducationTestCase(TestCase):
-    def setUp():
-        pass
+    def setUp(self):
+        # Initializes client to test endpoints
+        self.client = Client()
 
-    def tearDown():
-        pass
+        Account.objects.create(
+            email='test@gmail.com',
+            encryptedPass=bytes('password', 'utf-8'),
+            salt=bytes('salt', 'utf-8'),
+            phoneNum='1234567890',
+            displayTheme='D',
+            enabled2Factor=False,
+            isMentor=False,
+            isMentee=True
+        )
 
     """
     POST tests
     """
-    def test_post_education_to_account_succeeds():
+    def test_post_valid_education_to_account_succeeds(self):
         """
-        Passes if POST response status code is 201.
+        Makes request to /education/ endpoint
+
+        Passes if:
+            - New education row is created
+            - All education row data is correct.
+            - New education bridge row is created
+            - All education bridge data is correct.
+            - POST response status code is 201.
+        """
+
+        test_education = {
+            'degreeName': 'BS',
+            'collegeName': 'Hamburger University',
+            'optionalDescription': "I'll have you know I graduated top of my class, " \
+                                    "and I have over 300 confirmed happy customers."
+        }
+        response = self.client.post()
+
+    def test_post_education_creates_empty_description_if_missing(self):
+        """
+        Passes if:
+            - New education row has empty description
         """
         pass
 
-    def test_post_education_to_nonexistent_account_id_fails():
+    def test_post_education_to_nonexistent_account_id_fails(self):
         """
         Passes if POST response status code is 400.
         """
         pass
 
-    def test_post_education_to_null_account_id_fails():
+    def test_post_education_to_null_account_id_fails(self):
+        """
+        Passes if POST response status code is 400.
+        """
+        pass
+
+    def test_post_education_with_invalid_degree_name_fails(self):
+        """
+        Passes if POST response status code is 400.
+        """
+        pass
+
+    def test_post_education_with_invalid_college_name_fails(self):
+        """
+        Passes if POST response status code is 400.
+        """
+        pass
+
+    def test_post_education_missing_degree_name_fails(self):
+        """
+        Passes if POST response status code is 400.
+        """
+        pass
+
+    def test_post_education_missing_college_name_fails(self):
         """
         Passes if POST response status code is 400.
         """
