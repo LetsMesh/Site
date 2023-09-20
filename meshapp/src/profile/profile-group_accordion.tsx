d3cfe85d4fb0354d31efeb38d87c038a14004f01
Seisigh <alvinw.seisigh@gmail.com>
2023-09-19 20:25:21 -0700

    added plus button to accordion

--- meshapp/src/profile/profile-group_accordion.tsx
+++ meshapp/src/profile/profile-group_accordion.tsx
@@ -1,7 +1,8 @@
 import { useContext, useState } from "react";
 import ProfileAccordion from "./profile-accordion";
 import { createContext } from "react";
-import { Grid } from "@mui/material";
+import { Grid, IconButton } from "@mui/material";
+import ControlPointIcon from "@mui/icons-material/ControlPoint";
 
 //type of group accordion state
 type groupAccordionState = Array<{
@@ -56,25 +57,41 @@ export default function ProfileGroupAccordion(props: {
     setGroupState: setGroupAccordState,
   };
 
+  //function that adds new empty accordion
+  const addAccordion = () => {
+    let accordions = [
+      ...GroupAccordState,
+      { headerOne: "", headerTwo: "", descText: "" },
+    ];
+    setGroupAccordState(accordions);
+  };
+
   return (
     <GroupAccordContext.Provider
       value={value}
-      children={GroupAccordState.map((accordArgs, accordIndex) => {
-        return (
-          <ProfileAccordion
-            accordionIndex={accordIndex}
-            headerOne={accordArgs.headerOne}
-            headerTwo={accordArgs.headerTwo}
-            descPlaceholder={props.descPlaceholder}
-            descText={accordArgs.descText}
-            charLimit={props.charLimit}
-            headerOnePlaceholder={props.headerOnePlaceholder}
-            headerTwoPlaceholder={props.headerTwoPlaceholder}
-            headerOneOptions={props.headerOneOptions}
-            headerTwoOptions={props.headerTwoOptions}
-          />
-        );
-      })}
+      children={
+        <Grid xs={12} container flexDirection={"column"} alignItems={"center"}  >
+          <Grid container flexDirection={"column"} >
+          {GroupAccordState.map((accordArgs, accordIndex) => {
+            return (
+              <ProfileAccordion
+                accordionIndex={accordIndex}
+                headerOne={accordArgs.headerOne}
+                headerTwo={accordArgs.headerTwo}
+                descPlaceholder={props.descPlaceholder}
+                descText={accordArgs.descText}
+                charLimit={props.charLimit}
+                headerOnePlaceholder={props.headerOnePlaceholder}
+                headerTwoPlaceholder={props.headerTwoPlaceholder}
+                headerOneOptions={props.headerOneOptions}
+                headerTwoOptions={props.headerTwoOptions}
+              />
+            );
+          })}
+          </Grid>
+          <IconButton children={<ControlPointIcon sx={{fontSize:"30px", marginTop: "10px"}} />} onClick={addAccordion} />
+        </Grid>
+      }
     />
   );
 }
