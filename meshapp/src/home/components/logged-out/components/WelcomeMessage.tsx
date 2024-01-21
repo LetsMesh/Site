2a3ab85d4eadc8c9ae8d6a132ca08fd905ab211c
bluesimp1102 <jackismyname7@gmail.com>
2024-01-21 13:51:07 -0800

    change font size

--- meshapp/src/home/components/logged-out/components/WelcomeMessage.tsx
+++ meshapp/src/home/components/logged-out/components/WelcomeMessage.tsx
@@ -9,12 +9,12 @@ const WelcomeMessage = () => {
         textAlign={"center"}
         sx={{
           fontFamily: "cocogoose",
-          fontSize: "48px",
+          fontSize: "2.5em",
           padding: { lg: "0 100px", xs: "0 16px" },
         }}
       >
         Find Your Perfect Mentor or Mentee with{" "}
-        <span style={{ fontWeight: "700", fontSize: "60px" }}>mesh</span>
+        <span style={{ fontWeight: "700", fontSize: "1.2em" }}>mesh</span>
       </Typography>
       <Typography variant="body1" fontWeight={250} textAlign={"center"}>
         Unlock Your Potential with Expert Guidance and Support.
