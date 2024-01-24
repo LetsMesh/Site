71a6c4ad4656ea81e2b48c56f97a62ff0ef21ea8
bluesimp1102 <jackismyname7@gmail.com>
2024-01-24 14:51:23 -0800

    login authenticate
    return the account object if the user is logged in successfully instead of just the accountID

--- mesh/auth/views.py
+++ mesh/auth/views.py
@@ -20,7 +20,24 @@ def login_view(request):
         account: Account = AccountAuthenticationBackend().authenticate(request=request,email=email, password=password)
         if account:
             login(request, account)  # This creates the session
-            return JsonResponse({"message": "Access granted", "accountID": account.accountID}, status=200)
+            account = Account.objects.filter(accountID=account.accountID).select_related('settings').first()
+            if account:
+                account_data = {
+                    'accountID': account.accountID,
+                    'email': account.email,
+                    'phoneNum': account.phoneNum,
+                    'isMentor': account.isMentor,
+                    'isMentee': account.isMentee,
+                }
+                # Check if account settings exist and include them
+                if hasattr(account, 'settings'):
+                    account_data['settings'] = {
+                        'isVerified': account.settings.isVerified,
+                        'hasContentFilterEnabled': account.settings.hasContentFilterEnabled,
+                        'displayTheme': account.settings.displayTheme,
+                        'is2FAEnabled': account.settings.is2FAEnabled,
+                    }
+            return JsonResponse({"message": "Access granted", "is_logged_in": True, "account": account_data}, status=200)
         else:
             # Generic error message for security
             return JsonResponse({"error": "Invalid credentials"}, status=401)
