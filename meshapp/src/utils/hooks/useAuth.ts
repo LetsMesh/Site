272d7a69158186a4c4e2ac2d9a5508798346820e
bluesimp1102 <jackismyname7@gmail.com>
2024-01-23 18:16:06 -0800

    authentication hook (login and session)
    a hook for logging in, this hook send a request to our backend API to check and login with entered credentials

---
+++ meshapp/src/utils/hooks/useAuth.ts
@@ -0,0 +1,28 @@
+// hooks/useLogin.ts
+import { useState } from "react";
+import { apiAxiosInstance } from "../axios/axiosConfig";
+
+export const useLogin = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const login = async (email: String, password: String) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await apiAxiosInstance.post("/auth/login/", {
+        email: email,
+        password: password,
+      });
+
+      setIsLoading(false);
+      return response.data;
+    } catch (err: any) {
+      setError(err.response?.data?.error || "An error occurred");
+      setIsLoading(false);
+      return null;
+    }
+  };
+
+  return { login, isLoading, error };
+};
