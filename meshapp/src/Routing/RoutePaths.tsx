bcbe4bfaa816db9bc818e1d8ee4049a720879042
bluesimp1102 <jackismyname7@gmail.com>
2024-01-24 13:29:09 -0800

    rename some routes

--- meshapp/src/Routing/RoutePaths.tsx
+++ meshapp/src/Routing/RoutePaths.tsx
@@ -4,9 +4,10 @@
 export const paths = {
   home: "/",
   login_page: "/login",
-  forgot_password: "/forgot_password",
-  profile_swipe: "/profile_swipe",
+  forgot_password: "/forgot-password",
+  profile_swipe: "/profile-swipe",
   profile_page: "/profile",
   sign_up: "/sign_up",
   settings: "/settings",
+  messages: "/messages",
 };
