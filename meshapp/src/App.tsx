c52411fd2475c89cba5607009291f2f6b3d2d80d
bluesimp1102 <jackismyname7@gmail.com>
2024-01-24 13:23:40 -0800

    reorganize
    added AccountProvider and wrapped everything in it
    moved TwoFactorAuthReminders to pages Layout

--- meshapp/src/App.tsx
+++ meshapp/src/App.tsx
@@ -1,25 +1,22 @@
 import "./App.css";
-import React from "react";
 import AppRoutes from "./Routing/AppRoutes";
-import TwoFactorAuthReminders from "./two-factor-auth/two-factor-reminder";
 import ProfilePage from "./profile/profile-page";
 import { exampleProfile } from "./profile/tests/profile-examples";
 import { ThemeContextProvider } from "./theme/ThemeContextProvider";
 import { MainThemeProvider } from "./theme/MainThemeProvider";
 import { AccountProvider } from "./contexts/UserContext";
+import React from "react";
 
 function App() {
   return (
     <React.StrictMode>
-      <ThemeContextProvider>
-        <MainThemeProvider>
-          <AccountProvider>
-            {/* <TwoFactorAuthReminders /> */}
-            {/* <ProfilePage {...exampleProfile} /> */}
+      <AccountProvider>
+        <ThemeContextProvider>
+          <MainThemeProvider>
             <AppRoutes />
-          </AccountProvider>
-        </MainThemeProvider>
-      </ThemeContextProvider>
+          </MainThemeProvider>
+        </ThemeContextProvider>
+      </AccountProvider>
     </React.StrictMode>
   );
 }
