4a6fc6ee484c130863e320fdb1ccfe741e0ce367
bluesimp1102 <jackismyname7@gmail.com>
2024-01-29 11:30:17 -0800

    message timestamp to relative time

--- meshapp/src/components/Messages/ConversationCard.tsx
+++ meshapp/src/components/Messages/ConversationCard.tsx
@@ -14,6 +14,7 @@ import {
   useTheme,
 } from "@mui/material";
 import FolderIcon from "@mui/icons-material/Folder";
+import { relativeTimeFromDates } from "../../utils/utils/relativeTimestamp";
 
 interface ConversationComponentProps {
   conversation: Conversation;
@@ -101,7 +102,7 @@ const ConversationCard: React.FC<ConversationComponentProps> = ({
                     flexShrink: 0,
                   }}
                 >
-                  {new Date(msg.timestamp).toLocaleString()}
+                  {relativeTimeFromDates(new Date(msg.timestamp))}
                 </div>{" "}
               </>
             ) : (
