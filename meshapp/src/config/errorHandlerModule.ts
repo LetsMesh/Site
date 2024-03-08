51e53730f727cb4b1933be44aebac25612ac66ea
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2024-03-07 17:06:31 -0800

    removed redundant comments

--- meshapp/src/config/errorHandlerModule.ts
+++ meshapp/src/config/errorHandlerModule.ts
@@ -8,8 +8,6 @@ import Swal from 'sweetalert2'
  */
 export function errorHandler(error: AxiosError): void{
   if(error.response){
-    // The request was made and the server responded with a status code
-    // that falls out of the range of 2xx
     const responseData = error.response.data as { message:string };
     Swal.fire({
       icon: "error",
@@ -23,11 +21,9 @@ export function errorHandler(error: AxiosError): void{
     console.error("Error response header:", error.response.headers);
   }
   else if (error.request) {
-    //request was made but no response was received
     console.error("Error, request was made but no response was recieved.", error.request);
   }
   else {
-    // Something happened in setting up the request that triggered an Error
     console.error('Error during setting up the request.', error.message);
   }
   console.log(error.config);
