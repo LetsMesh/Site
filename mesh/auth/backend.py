ad83a74a20ecd0f8394a1024767a1b4f6ebba849
bluesimp1102 <jackismyname7@gmail.com>
2024-04-08 12:10:08 -0700

    #42 login with session

---
+++ mesh/auth/backend.py
@@ -0,0 +1,102 @@
+# in auth folder: backend.py  (auth.backend)
+
+from django.contrib.auth.backends import BaseBackend
+from mesh.accounts.models import Account
+
+from mesh.accounts.views import decrypt
+
+class AccountAuthenticationBackend(BaseBackend):
+    """
+    A custom authentication backend that uses email and password for authentication.
+
+    This backend extends Django's BaseBackend and is specific to the Account model.
+    It authenticates users based on their email and password, using custom logic
+    defined for the Account model. Additionally, it includes a method to retrieve 
+    account data along with its settings.
+    """
+
+    def authenticate(self, request, email=None, password=None):
+        """
+        Authenticates a user based on email and password.
+
+        Args:
+            request: The HTTP request object. Not directly used in this method but is
+                     required by the Django authentication backend interface.
+            email (str, optional): The email of the user attempting to log in.
+            password (str, optional): The password of the user.
+
+        Returns:
+            Account instance if authentication is successful, None otherwise.
+
+        This method attempts to find an Account instance based on the provided email.
+        If found, it decrypts the provided password and compares it against the stored
+        encrypted password. If the credentials match, the Account instance is returned.
+        Otherwise, None is returned to indicate failed authentication.
+        """
+        try:
+            account = Account.objects.get(email=email)
+            if account.encryptedPass == decrypt(password, account.salt):
+                return account
+            else:
+                return None
+        except Account.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        """
+        Retrieves a user based on a user ID.
+
+        Args:
+            user_id: The ID of the user to retrieve.
+
+        Returns:
+            Account instance if a user with the given ID exists, None otherwise.
+
+        This method is used by Django's authentication system to fetch the user
+        instance associated with a user ID. It is particularly useful for session
+        validation and other authentication-related operations.
+        """
+        try:
+            return Account.objects.get(pk=user_id)
+        except Account.DoesNotExist:
+            return None
+        
+    def get_account_data(self, accountID):
+        """
+        Retrieves account data along with its settings based on the account ID.
+
+        Args:
+            accountID: The ID of the account to retrieve.
+
+        Returns:
+            A dictionary containing the account data and its settings if they exist,
+            otherwise None.
+
+        If the account does not have settings, a default Settings instance is created.
+        """
+        from django.db import transaction
+        from mesh.accountSettings.models import Settings
+        with transaction.atomic():
+            account_with_settings = Account.objects.filter(accountID=accountID).select_related('settings').first()
+            if account_with_settings:
+                # Create default settings if they do not exist
+                if not hasattr(account_with_settings, 'settings'):
+                    Settings.objects.create(accountID=account_with_settings)
+
+                account_data = {
+                    'accountID': account_with_settings.accountID,
+                    'email': account_with_settings.email,
+                    'phoneNum': account_with_settings.phoneNum,
+                    'isMentor': account_with_settings.isMentor,
+                    'isMentee': account_with_settings.isMentee,
+                    'settings': {
+                        'isVerified': account_with_settings.settings.isVerified,
+                        'hasContentFilterEnabled': account_with_settings.settings.hasContentFilterEnabled,
+                        'displayTheme': account_with_settings.settings.displayTheme,
+                        'is2FAEnabled': account_with_settings.settings.is2FAEnabled,
+                    }
+                }
+
+                return account_data
+        
+        return None
---
+++ mesh/auth/views.py
@@ -0,0 +1,79 @@
+# in auth folder: views.py  (auth.views)
+
+import json
+from django.http import JsonResponse, HttpResponseRedirect
+from django.contrib.auth import login, logout
+from django.views.decorators.http import require_POST,require_GET
+from django.views.decorators.csrf import ensure_csrf_cookie
+from mesh.accounts.models import Account
+from .backend import AccountAuthenticationBackend
+
+@require_POST
+def login_view(request):
+    """
+    Handles user login requests.
+
+    This view expects a POST request with 'email' and 'password' in the JSON body. It authenticates
+    the user using a custom authentication backend, creates a session upon successful authentication,
+    and returns user details along with login status.
+
+    Returns:
+        JsonResponse: A response with a success message and user details if authentication is successful,
+                      otherwise an error message.
+    """
+    try:
+        data = json.loads(request.body)
+        email = data.get("email")
+        password = data.get("password")
+
+        account_backend = AccountAuthenticationBackend()
+        # Query to check if user exists
+        account: Account = account_backend.authenticate(request=request,email=email, password=password)
+        if account:
+            login(request, account)  # This creates the session
+            account_data = account_backend.get_account_data(account.accountID)
+            return JsonResponse({"message": "Access granted", "is_logged_in": True, "account": account_data}, status=200)
+        else:
+            # Generic error message for security
+            return JsonResponse({"error": "Invalid credentials"}, status=401)
+    except json.JSONDecodeError:
+        return JsonResponse({"error": "Invalid JSON format"}, status=400)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+
+def logout_view(request):
+    """
+    Handles user logout requests.
+
+    This view expects a POST request, logs out the user by ending the session, and
+    redirects to the home page.
+
+    Returns:
+        HttpResponseRedirect: A response that redirects to the home page after successful logout.
+    """
+    logout(request)
+    import os
+    return HttpResponseRedirect(os.environ.get('WEB_URL', 'http://localhost:3000'))
+
+@ensure_csrf_cookie
+@require_GET
+def session_view(request):
+    """
+    Handles session check requests.
+
+    This view expects a GET request. It checks if the user is authenticated and, if so,
+    retrieves their account details including settings. It's decorated with `ensure_csrf_cookie`
+    to ensure CSRF protection.
+
+    Returns:
+        JsonResponse: A response with the user's account details if authenticated, or an error message.
+    """
+    if request.user.is_authenticated:
+        account = AccountAuthenticationBackend().get_account_data(request.user.accountID)
+        if account:
+            return JsonResponse({'is_logged_in': True, 'account': account}, status=200)
+        else:
+            return JsonResponse({'is_logged_in': False, 'error': 'Account not found'}, status=404)
+    else:
+        return JsonResponse({'is_logged_in': False}, status=401)
+
