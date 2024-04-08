63e1dc87d4bf69a0351f050f757b4f9ea46022e0
bluesimp1102 <jackismyname7@gmail.com>
2024-04-08 12:09:46 -0700

    #42 add last_login field for account

--- mesh/accounts/models.py
+++ mesh/accounts/models.py
@@ -1,5 +1,6 @@
 # in accounts folder: (accounts.models) models.py
 from django.db import models
+from django.utils import timezone
 
 class Account(models.Model):
     accountID = models.AutoField(primary_key=True)
@@ -14,7 +15,13 @@ class Account(models.Model):
     otp_base32 = models.CharField(
         max_length=200, null=True, blank=True
     )  # One Time Password's secret key
+    last_login = models.DateTimeField(default=timezone.now)
 
+    @property
+    def is_authenticated(self):
+        # Assuming all Account instances represent authenticated users
+        return True
+    
     def __str__(self) -> str:
         return str(self.accountID)
     
