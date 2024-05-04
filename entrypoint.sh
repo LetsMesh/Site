33783ce8c23e52b9ad28ffc81bb0a255a5f09f88
bluesimp1102 <jackismyname7@gmail.com>
2024-05-03 21:19:35 -0700

    #321 add .dockerignore and remove testing on docker container
    - added .dockerignore to ignore .env files and /node_modules folders when copying to the container
    - remove running tests on backend container startup to let it start up faster

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,5 @@ set -e
 pipenv run python manage.py makemigrations
 pipenv run python manage.py migrate
 
-pipenv run python manage.py test --pattern="*_tests.py"
-
 # Start Daphne server
 exec pipenv run daphne -b 0.0.0.0 -p 8000 mesh.asgi:application
