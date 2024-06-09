72569e574fe56aec081a3e54a3fb7ffaa38a5a14
bluesimp1102 <jackismyname7@gmail.com>
2024-06-09 13:28:05 -0500

    feat: user context to use across the site

---
+++ meshapp/src/contexts/UserContext.tsx
@@ -0,0 +1,66 @@
+import React, {
+  createContext,
+  useState,
+  useContext,
+  useEffect,
+  ReactNode,
+} from "react";
+import { axiosInstance } from "../config/axios-config";
+import { Account } from "../types/account";
+import { LoadingState } from "../types/loading";
+
+interface AccountContextType {
+  account: Account | null;
+  updateAccount: React.Dispatch<React.SetStateAction<Account | null>>;
+  loadingState: LoadingState;
+}
+
+const AccountContext = createContext<AccountContextType | undefined>(undefined);
+
+interface AccountProviderProps {
+  children: React.ReactNode;
+}
+
+export const AccountProvider: React.FC<AccountProviderProps> = ({
+  children,
+}) => {
+  const [account, setAccount] = useState<Account | null>(null);
+  const [loadingState, setLoadingState] =
+    useState<LoadingState>("initializing");
+
+  useEffect(() => {
+    setLoadingState("loading");
+    const checkSession = async () => {
+      try {
+        const response = await axiosInstance.get("/auth/session/");
+        if (response.data.is_logged_in) {
+          setAccount(response.data.account);
+        } else {
+          setAccount(null);
+        }
+      } catch (error) {
+        console.error("Session check failed", error);
+      } finally {
+        setLoadingState("completed");
+      }
+    };
+
+    checkSession();
+  }, []);
+
+  return (
+    <AccountContext.Provider
+      value={{ account, updateAccount: setAccount, loadingState }}
+    >
+      {children}
+    </AccountContext.Provider>
+  );
+};
+
+export const useAccountContext = (): AccountContextType => {
+  const context = useContext(AccountContext);
+  if (context === undefined) {
+    throw new Error("useAccountContext must be used within an AccountProvider");
+  }
+  return context;
+};
