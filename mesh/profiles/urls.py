a95f4a4c9f1cc27e1fbb7005a5d6010d6f51e619
bluesimp1102 <jackismyname7@gmail.com>
2024-06-15 12:13:54 -0700

    refactor: use include to remove redundancy

--- mesh/profiles/urls.py
+++ mesh/profiles/urls.py
@@ -1,17 +1,18 @@
-from django.urls import re_path, path
+from django.urls import include, path
 
 from . import views as profile_views
 
 urlpatterns = [
     
-    # GET /profiles/:account-id/profile-picture
-    # POST /profiles/:account-id/profile-picture
-    # PATCH /profiles/:account-id/profile-picture
-    # DELETE /profiles/:account-id/profile-picture
-    path('<int:account_id>/profile-picture/', profile_views.ProfilePicturesView.as_view(), name="profile-picture"),
+    path('<int:account_id>/', include([
+        # GET    /profiles/:account-id
+        # GET    /profiles/:account-id?fields=
+        # PATCH  /profiles/:account-id
+        path('', profile_views.ProfileDetailsView.as_view(), name='profile-details'),
 
-    # GET    /profiles/:account-id
-    # GET    /profiles/:account-id?fields=
-    # PATCH  /profiles/:account-id
-    path('<int:account_id>/', profile_views.ProfileDetailsView.as_view(), name='profile-details'),
+        # GET    /profiles/:account-id/profile-picture
+        # POST   /profiles/:account-id/profile-picture
+        # DELETE /profiles/:account-id/profile-picture
+        path('profile-picture/', profile_views.ProfilePicturesView.as_view(), name="profile-picture"),
+    ])),
 ]
